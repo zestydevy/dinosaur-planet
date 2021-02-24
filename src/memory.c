f50273926e5e42649394cef2ffe8f90bc0418997
zestydevy <zestydevy@gmail.com>
2021-02-24 04:41:30 -0600

    memory.c

---
+++ src/memory.c
@@ -0,0 +1,49 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/wipe_mem_past_bss.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/some_mem_func.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_building_struct_for_romcopy.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_80016BB4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_80016E68.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_80016EE0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_8001707C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_80017254.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_800172E8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_80017344.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_8001753C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_800175D4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_80017610.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_80017674.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_80017790.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_800177B4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_800178CC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_800179A4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_800179C4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_800179E4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_80017A04.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/some_memory_monitor.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_80017B3C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/memory/func_80017BA8.s")
