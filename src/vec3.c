ac4d600089e9ac998c595cd72b9be4e7006b784e
farisawan-2000 <farisawan.2000@gmail.com>
2021-03-01 22:47:58 -0500

    fully matched vec3.c

--- src/vec3.c
+++ src/vec3.c
@@ -1,19 +1,74 @@
 #include "common.h"
+extern f32 _sqrtf(f32);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/vec3/func_80016580.s")
+f32 vec3_dot(Vec3f *v1, Vec3f *v2) {
+    return (v2->z * v1->z) + ((v1->x * v2->x) + (v1->y * v2->y));
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/vec3/vec3_hypotenouse.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/vec3/vec3_squared.s")
+f32 vec3_hypotenouse(Vec3f *v) {
+    return _sqrtf((v->z * v->z) + ((v->x * v->x) + (v->y * v->y)));
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/vec3/func_80016620.s")
+f32 vec3_squared(Vec3f *v) {
+    return (v->z * v->z) + ((v->x * v->x) + (v->y * v->y));
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/vec3/func_80016694.s")
+void func_80016620(Vec3f *v1, Vec3f *v2, Vec3f *dst) {
+    dst->x = (v1->y * v2->z) - (v1->z * v2->y);
+    dst->y = (v1->z * v2->x) - (v1->x * v2->z);
+    dst->z = (v1->x * v2->y) - (v1->y * v2->x);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/vec3/possible_vec3_normalize.s")
+void func_80016694(Vec3f *v1, Vec3f *v2, Vec3f *dst) {
+    dst->x = (v1->y * v2->z) - (v1->z * v2->y);
+    dst->y = (v1->z * v2->x) - (v1->x * v2->z);
+    dst->z = (v1->x * v2->y) - (v1->y * v2->x);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/vec3/vec3_c_AminB.s")
+f32 possible_vec3_normalize(Vec3f *v) {
+    f32 mag;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/vec3/vec3_AplusBtimesC.s")
+    mag = _sqrtf((v->z * v->z) + ((v->x * v->x) + (v->y * v->y)));
+    if (mag != 0.0f) {
+    	f32 mag_inverse = 1.0f / mag;
+        v->x *= mag_inverse;
+        v->y *= mag_inverse;
+        v->z *= mag_inverse;
+    }
+    return mag;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/vec3/func_80016820.s")
+void vec3_c_AminB(Vec3f *v1, Vec3f *v2, Vec3f *dst) {
+    dst->x = v1->x - v2->x;
+    dst->y = v1->y - v2->y;
+    dst->z = v1->z - v2->z;
+}
+
+void vec3_AplusBtimesC(Vec3f *v1, Vec3f *v2, f32 C, Vec3f *dst) {
+    dst->x = v1->x + (C * v2->x);
+    dst->y = v1->y + (C * v2->y);
+    dst->z = v1->z + (C * v2->z);
+}
+
+void func_80016820(Vec3f *v1, Vec3f *v2, Vec3f *dst) {
+    f32 mag_square = (v2->z * v1->z) + ((v1->x * v2->x) + (v1->y * v2->y));
+
+    if (mag_square > 0.0f) {
+        dst->x = v2->x;
+        dst->y = v2->y;
+        dst->z = v2->z;
+    } else {
+	    mag_square = mag_square * (-2.0f);
+
+	    dst->x = v1->x;
+	    dst->y = v1->y;
+	    dst->z = v1->z;
+	    dst->x *= mag_square;
+	    dst->y *= mag_square;
+	    dst->z *= mag_square;
+	    dst->x = v2->x + dst->x;
+	    dst->y = v2->y + dst->y;
+	    dst->z = v2->z + dst->z;
+	}
+}
