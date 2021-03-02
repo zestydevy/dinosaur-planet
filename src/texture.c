4b7b8ff47f9d5577e0df9ce2c2c2148b76574635
Faris Awan <58312479+farisawan-2000@users.noreply.github.com>
2021-03-02 17:38:38 -0500

    queue.c matching, rodata integration, documentation (#29)
    * decomped game_tick; matched update_PlayerPosBuffer
    
    * matched update_PlayerPosBuffer better
    
    * queue decomp
    
    * fully matched vec3.c
    
    * added first_diff, fixed symbol mismatch
    
    * matched texture.c
    
    * reverted vec3 because of PR
    
    * newline in vec3 revert
    
    * matched texture.c better
    
    * split ucode data; fixed texture file discrepancy
    
    * queue.c matching
    
    * tiny comment removal
    
    Co-authored-by: farisawan-2000 <farisawan.2000@gmail.com>

--- src/texture.c
+++ src/texture.c
@@ -1,7 +1,104 @@
 #include "common.h"
 
-// TODO: turn this into gSPSegment (is the gbi.h old?)
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/init_textures.s")
 
-void func_8003CC50(Gfx **arg0, u32 arg1, s32 arg2) {
-	gDma1p((*arg0)++, 0xDB, 0x80000000 + arg2, arg1 * 4, 0x06);
-}
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003CD6C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003CD7C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/texture_load.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003D14C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003D254.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003DA40.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003DB5C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003DB7C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003DBCC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003DC04.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003E18C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003E648.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003E904.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003E960.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003E9B4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003E9D0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003E9F0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003EAC0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003EBD4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003EC8C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003ED00.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003EF30.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003F074.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003F1EC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003F2C4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/NOTosSetTime.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003F4C0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003F660.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003F928.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003FA88.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003FB88.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003FC3C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003FCBC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003FD48.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003FE70.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_800402F8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_80040378.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_800403AC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_80040468.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_80040590.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_80040754.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_80040870.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_80040920.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_800409D0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_80040A04.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_80040A94.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_80040B34.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_80040B8C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_80040CD0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/texture/func_80040EFC.s")
+
