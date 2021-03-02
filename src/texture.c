ccb7443ffa2edad6ffaa493862d2b1e571277500
farisawan-2000 <farisawan.2000@gmail.com>
2021-03-02 14:18:38 -0500

    matched texture.c better

--- src/texture.c
+++ src/texture.c
@@ -3,5 +3,5 @@
 // TODO: turn this into gSPSegment (is the gbi.h old?)
 
 void func_8003CC50(Gfx **arg0, u32 arg1, s32 arg2) {
-	gDma1p((*arg0)++, 0xDB, &((u8 *)0x80000000)[arg2], arg1 * 4, 0x06);
+	gDma1p((*arg0)++, 0xDB, 0x80000000 + arg2, arg1 * 4, 0x06);
 }
