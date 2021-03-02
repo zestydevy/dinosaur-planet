18721e4f450ead11589c7ea1d9be77013827c0d0
farisawan-2000 <farisawan.2000@gmail.com>
2021-03-01 23:06:51 -0500

    added first_diff, fixed symbol mismatch

--- src/texture.c
+++ src/texture.c
@@ -1,3 +1,15 @@
 #include "common.h"
 
+// This function is
+// is not adding incrementing arg0 like it should
+
+#if 1
 #pragma GLOBAL_ASM("asm/nonmatchings/texture/func_8003CC50.s")
+#else
+void _func_8003CC50(Gfx **arg0, u32 arg1, s32 arg2) {
+    gSPSegment(*arg0++, arg1, &((u8 *)0x80000000)[arg2]);
+
+    // lower level gSPSegment, in case
+	// gDma1p(*arg0++, 0xDB, &((u8 *)0x80000000)[arg2], arg1 * 4, 0x06);
+}
+#endif
