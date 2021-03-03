60f08d30ea21a1b9bc0118e8b5c4fe2b834df96e
SunlitSpace542 <35207254+Sunlitspace542@users.noreply.github.com>
2021-03-03 13:57:41 -0500

    match some_crash_setter, func_8000BDE8, and func_8000C3BC

--- src/dll.c
+++ src/dll.c
@@ -4,9 +4,15 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/dll/func_8000BD1C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/dll/some_crash_setter.s")
+void some_crash_setter(s32 arg0, s32 arg1) {
+    D_800A7D14 = arg1;
+    D_800A7D10 = arg0;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/dll/func_8000BDE8.s")
+s32 func_8000BDE8(s32 *arg0) {
+    *arg0 = (s32) D_800A7D14;
+    return D_800A7D10;
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/dll/dll_load_deferred.s")
 
@@ -16,7 +22,11 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/dll/func_8000C258.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/dll/func_8000C3BC.s")
+s32 func_8000C3BC(void) { // This function is quite strange
+    //*NULL = (u8)0; // It's supposed to to this.... (which would crash the game)
+    *(volatile char *)0 = 0;  // but IDO optimizes to this
+    return 0;
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/dll/dll_load_from_tab.s")
 
