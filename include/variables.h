e9a542723e0b5528984ba97316301caf179a6b01
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2021-03-03 22:34:02 -0500

    Match most of set_framebuffer_pointers

--- include/variables.h
+++ include/variables.h
@@ -181,9 +181,16 @@ extern OSViMode OSViMode_custom;
 extern u32 CurrentResolutionH[2]; //picked from resolutionArray[video_mode % 7]
 extern u32 CurrentResolutionV[2]; //one for each FB, apparently
 extern void* framebufferPointers[2]; //{FBaddr,FBaddr*H*V*2}
+
+extern u32 framebufferAddress_ExpPak;
+extern u32 framebufferAddress_NoExpPak;
+
+extern u32* framebufferStart;
+extern u32* frameBufferEnd;
 extern u32 framebufferChoice; 
 extern u32 video_mode;
 extern float aspectRatioFloat; //1.121212 for PAL, 1.333 for NTSC/MPAL.
+extern u32* resolutionArray;
 
 
 extern u8 delayByte; //60/int(x) FPS x<=6
