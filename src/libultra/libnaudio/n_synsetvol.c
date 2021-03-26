f71165cf330d8ee547dddd394f555f8b881ebec1
zestydevy <zestydevy@gmail.com>
2021-03-25 22:30:35 -0500

    a tiny bit more of libnaudio

---
+++ src/libultra/libnaudio/n_synsetvol.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/libnaudio/n_synsetvol/n_alSynSetVol.s")
--- src/segment_6CD50.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_6CD50/func_8006C150.s")
