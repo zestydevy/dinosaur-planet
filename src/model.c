ffe123f892e8926d3ae9dfd64e90f46b0b162401
N.E.C <beholdnec@gmail.com>
2021-04-06 21:59:14 -0700

    model_load_create_instance: Initial implementation

--- src/model.c
+++ src/model.c
@@ -4,13 +4,41 @@
 
 typedef struct
 {
+/*0000*/    void *texture;
+/*0004*/    u32 unk_0x4;
+} ModelTexture;
+
+typedef struct
+{
+/*0000*/    u8 unk_0x0;
+/*0001*/    u8 unk_0x1[0x10 - 0x1];
+} ModelUnk0x8;
+
+typedef struct
+{
     // TODO
-/*0000*/    u8 unk_0x0[0x1c];
+/*0000*/    ModelTexture *textures;
+/*0004*/    void *unk_0x4;
+/*0008*/    ModelUnk0x8 *unk_0x8;
+/*000C*/    void *unk_0xc;
+/*0010*/    void **anims;
+/*0014*/    void *unk_0x14;
+/*0018*/    void *unk_0x18;
 /*001C*/    u32 unk_0x1c;
-/*0020*/    u32 unk_0x20;
-/*0024*/    u8 unk_0x24[0x50 - 0x24];
-/*0050*/    u32 unk_0x50;
-/*0054*/    u32 unk_0x54;
+/*0020*/    void *unk_0x20;
+/*0024*/    u32 unk_0x24;
+/*0028*/    void *unk_0x28;
+/*002C*/    void *unk_0x2c;
+/*0030*/    u32 unk_0x30;
+/*0034*/    void *unk_0x34;
+/*0038*/    void *unk_0x38;
+/*003C*/    void *unk_0x3c;
+/*0040*/    u32 unk_0x40;
+/*0044*/    u32 unk_0x44;
+/*0048*/    u32 unk_0x48;
+/*004C*/    u32 unk_0x4c;
+/*0050*/    void *unk_0x50;
+/*0054*/    void *unk_0x54;
 /*0058*/    u32 unk_0x58;
 /*005C*/    u32 unk_0x5c;
 /*0060*/    u16 unk_0x60;
@@ -18,20 +46,31 @@ typedef struct
 /*0064*/    u16 unk_0x64;
 /*0066*/    s16 unk_0x66;
 /*0068*/    s16 unk_0x68;
-/*006a*/    u16 unk_0x6a;
+/*006A*/    s16 modelId;
 /*006C*/    u8 unk_0x6c;
 /*006D*/    u8 unk_0x6d;
 /*006E*/    u8 unk_0x6e;
 /*006F*/    u8 unk_0x6f;
 /*0070*/    u8 unk_0x70;
 /*0071*/    u8 unk_0x71;
+/*0072*/    u8 refCount;
+/*0073*/    u8 textureCount;
 } Model;
 
 typedef struct
 {
-    Model *model;
     // TODO
+/*0000*/    Model *model;
+/*0004*/    u8 unk_0x4[0x28 - 0x4];
+/*0028*/    u32 unk_0x28;
+/*002C*/    u32 unk_0x2c;
 } ModelInstance;
+
+typedef struct
+{
+/*0000*/    s32 id;
+/*0004*/    Model *model;
+} LoadedModel;
 
 typedef struct
 {
@@ -45,12 +84,243 @@ typedef struct
 /*0018*/    u32 unk_0x18;
 } ModelStats;
 
+typedef struct
+{
+/*0000*/    s16 unk_0x0;
+/*0002*/    s16 unk_0x2;
+/*0004*/    s16 unk_0x4;
+/*0006*/    s16 unk_0x6;
+/*0008*/    u32 uncompressedSize; // CAUTION: little-endian
+} ModelHeader;
+
 
 #pragma GLOBAL_ASM("asm/nonmatchings/model/init_models.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/model/func_80017D2C.s")
 
+#if 1
 #pragma GLOBAL_ASM("asm/nonmatchings/model/model_load_create_instance.s")
+#else
+#define MAX_LOADED_MODELS 70
+extern void *gAuxBuffer;
+extern s32 INT_800b17b4;
+extern s32 gNumLoadedModels;
+extern s32 *PTR_DAT_800b17a8;
+extern s32 gNumModelsTabEntries;
+extern s32 *gFile_MODELS_TAB;
+extern LoadedModel *gLoadedModels;
+void func_80018B14(Model *model);
+u32 read_le32(u32 *p);
+ModelInstance *createModelInstance(Model *model, u32 flags, s32 initial);
+u32 modanim_load(Model *model, u32 id, void *modanim);
+ModelInstance *_model_load_create_instance(s32 id, u32 flags)
+{
+    s32 slot;
+    u32 offset;
+    u32 *tab;
+    u32 loadSize;
+    u32 animRemapSize;
+    u32 modelSize;
+    u32 uncompressedSize;
+    u8* compressedData;
+    int isNewSlot;
+    u32 uVar8;
+    ModelHeader *header;
+    s16 unk_0x0;
+    s16 unk_0x2_aligned;
+    s16 unk_0x4;
+    s32 i;
+    int fail;
+    Model *model;
+    ModelInstance *modelInst;
+    void *modanim;
+
+    if (id < 0) {
+        id = -id;
+    } else {
+        read_file_region(MODELIND_BIN, gAuxBuffer, id * sizeof(s16), 8);
+        id = *(s16*)gAuxBuffer;
+    }
+
+    // Check to see if model is already loaded
+    for (i = 0; i < gNumLoadedModels; i++)
+    {
+        if (id == gLoadedModels[i].id)
+        {
+            model = gLoadedModels[i].model;
+            modelInst = createModelInstance(model, flags, 0);
+            if (modelInst != NULL)
+            {
+                model->refCount++;
+
+                model_setup_anim_playback(modelInst, modelInst->unk_0x28);
+                if (modelInst->unk_0x2c != 0) {
+                    model_setup_anim_playback(modelInst, modelInst->unk_0x2c);
+                }
+            }
+
+            return modelInst;
+        }
+    }
+
+    if (gNumModelsTabEntries <= id) {
+        id = 0;
+    }
+
+    isNewSlot = INT_800b17b4 < 1;
+    if (isNewSlot) {
+        slot = gNumLoadedModels;
+        gNumLoadedModels++;
+    } else {
+        INT_800b17b4--;
+        slot = PTR_DAT_800b17a8[INT_800b17b4];
+    }
+
+    tab = &gFile_MODELS_TAB[id];
+    offset = *tab;
+    loadSize = tab[1] - offset;
+    read_file_region(MODELS_BIN, gAuxBuffer, offset, 0x10);
+
+    header = gAuxBuffer;
+    unk_0x0 = header->unk_0x0;
+    unk_0x4 = header->unk_0x4;
+    unk_0x2_aligned = align_8(header->unk_0x2);
+    uncompressedSize = read_le32(&header->uncompressedSize);
+    animRemapSize = model_load_anim_remap_table(id, unk_0x4, unk_0x0);
+    modelSize = animRemapSize + uncompressedSize + 500;
+
+    model = malloc(modelSize, 9, 0);
+    if (!model) {
+        if (isNewSlot) {
+            gNumLoadedModels--;
+        } else {
+            INT_800b17b4++;
+        }
+
+        return NULL;
+    }
+
+    compressedData = (u8*)model + modelSize - loadSize - 0x10;
+    uVar8 = (u32)compressedData & 0xf;
+    if ((s32)compressedData < 0 && uVar8 != 0) {
+        uVar8 -= 0x10;
+    }
+    compressedData -= uVar8;
+
+    read_file_region(MODELS_BIN, compressedData, offset, loadSize);
+    inflate(compressedData + 8, model);
+
+    // Convert offsets to pointers
+    *(u32*)&model->textures += (u32)model;
+    *(u32*)&model->unk_0x4 += (u32)model;
+    *(u32*)&model->unk_0x8 += (u32)model;
+    *(u32*)&model->unk_0x28 += (u32)model;
+    *(u32*)&model->unk_0x18 += (u32)model;
+    *(u32*)&model->unk_0x14 += (u32)model;
+    *(u32*)&model->unk_0xc += (u32)model;
+    if (model->unk_0x3c != NULL) {
+        *(u32*)&model->unk_0x3c += (u32)model;
+    }
+    if (model->unk_0x38 != NULL) {
+        *(u32*)&model->unk_0x38 += (u32)model;
+    }
+    if (model->unk_0x1c != NULL) {
+        *(u32*)&model->unk_0x1c += (u32)model;
+    }
+
+    model->anims = NULL;
+
+    model->unk_0x24 = 0;
+
+    if (model->unk_0x20 != NULL) {
+        *(u32*)&model->unk_0x20 += (u32)model;
+        if (model->unk_0x50 != NULL) {
+            *(u32*)&model->unk_0x50 += (u32)model;
+        }
+        if (model->unk_0x54 != NULL) {
+            *(u32*)&model->unk_0x54 += (u32)model;
+        }
+    } else {
+        model->unk_0x50 = NULL;
+        model->unk_0x54 = NULL;
+    }
+
+    if (model->unk_0x2c != NULL) {
+        *(u32*)&model->unk_0x2c += (u32)model;
+    }
+    if (model->unk_0x34 != NULL) {
+        *(u32*)&model->unk_0x34 += (u32)model;
+    }
+
+    model->unk_0x68 = unk_0x2_aligned + 0x90;
+    model->refCount = 1;
+    model->modelId = id;
+    model->unk_0x71 &= 0xbf;
+    model->unk_0x66 = unk_0x0;
+
+    if (unk_0x4 != 0) {
+        model->unk_0x71 |= 0x40;
+    }
+
+    fail = FALSE;
+    for (i = 0; i < model->textureCount; i++)
+    {
+        void *texture = texture_load(-((u32)model->textures[i].texture | 0x8000));
+        model->textures[i].texture = texture;
+        if (!model->textures[i].texture) {
+            fail = TRUE;
+        }
+    }
+
+    if (fail) {
+        goto bail;
+    }
+
+    for (i = 0; i < model->unk_0x70; i++)
+    {
+        if (model->unk_0x8[i].unk_0x0 != 0xff && model->textureCount <= model->unk_0x8[i].unk_0x0) {
+            goto bail;
+        }
+    }
+
+    func_800186CC(model);
+
+    modanim = align_8((u32)model + uncompressedSize);
+
+    if (modanim_load(model, id, modanim) != 0) {
+        goto bail;
+    }
+
+    modelInst = createModelInstance(model, flags, 1);
+    if (!modelInst) {
+        goto bail;
+    }
+
+    model_setup_anim_playback(modelInst, modelInst->unk_0x28);
+    if (modelInst->unk_0x2c != 0) {
+        model_setup_anim_playback(modelInst, modelInst->unk_0x2c);
+    }
+
+    gLoadedModels[slot].id = id;
+    gLoadedModels[slot].model = model;
+
+    if (gNumLoadedModels >= MAX_LOADED_MODELS) {
+        goto bail;
+    }
+
+    return modelInst;
+
+bail:
+    if (isNewSlot) {
+        gNumLoadedModels--;
+    } else {
+        INT_800b17b4++;
+    }
+
+    func_80018B14(model);
+    return NULL;
+}
+#endif
 
 #pragma GLOBAL_ASM("asm/nonmatchings/model/createModelInstance.s")
 
