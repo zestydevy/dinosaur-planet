94af99ce328758326ac6462100b9b6eee5f6b83d
N.E.C <beholdnec@gmail.com>
2021-04-29 10:34:08 -0700

    dl_segment

--- src/segment_3D850.c
+++ src/segment_3D850.c
@@ -2,6 +2,6 @@
 
 // TODO: turn this into gSPSegment (is the gbi.h old?)
 
-void func_8003CC50(Gfx **arg0, u32 arg1, s32 arg2) {
-	gDma1p((*arg0)++, 0xDB, 0x80000000 + arg2, arg1 * 4, 0x06);
+void dl_segment(Gfx **gdl, u32 segment, void *base) {
+	gSPSegment((*gdl)++, segment, OS_K0_TO_PHYSICAL(base));
 }
