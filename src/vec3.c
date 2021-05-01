39bce974a9e85cb4971c655474a3af7349950064
Ethan Lafrenais <Francessco121@users.noreply.github.com>
2021-05-01 07:44:26 -0400

    Fix vec3.c style issues (#65)

--- src/vec3.c
+++ src/vec3.c
@@ -5,28 +5,28 @@ f32 _sqrtf(f32 f);
 /**
  * Returns the dot product of v1 and v2.
  */
-f32 vec3_dot_product(Vec3f* v1, Vec3f* v2) {
+f32 vec3_dot_product(Vec3f *v1, Vec3f *v2) {
     return v2->z * v1->z + (v1->x * v2->x + v1->y * v2->y);
 }
 
 /**
  * Returns the length of v.
  */
-f32 vec3_length(Vec3f* v) {
+f32 vec3_length(Vec3f *v) {
     return _sqrtf(v->z * v->z + (v->x * v->x + v->y * v->y));
 }
 
 /**
  * Returns the length of v squared.
  */
-f32 vec3_length_squared(Vec3f* v) {
+f32 vec3_length_squared(Vec3f *v) {
     return v->z * v->z + (v->x * v->x + v->y * v->y);
 }
 
 /**
  * Computes the cross product of v1 and v2 and stores it in result.
  */ 
-void vec3_cross_product(Vec3f* v1, Vec3f* v2, Vec3f* result) {
+void vec3_cross_product(Vec3f *v1, Vec3f *v2, Vec3f *result) {
     result->x = v1->y * v2->z - v1->z * v2->y;
     result->y = v1->z * v2->x - v1->x * v2->z;
     result->z = v1->x * v2->y - v1->y * v2->x;
@@ -35,7 +35,7 @@ void vec3_cross_product(Vec3f* v1, Vec3f* v2, Vec3f* result) {
 /**
  * Computes the cross product of v1 and v2 and stores it in result.
  */ 
-void vec3_cross_product_2(Vec3f* v1, Vec3f* v2, Vec3f* result) {
+void vec3_cross_product_2(Vec3f *v1, Vec3f *v2, Vec3f *result) {
     result->x = v1->y * v2->z - v1->z * v2->y;
     result->y = v1->z * v2->x - v1->x * v2->z;
     result->z = v1->x * v2->y - v1->y * v2->x;
@@ -44,7 +44,7 @@ void vec3_cross_product_2(Vec3f* v1, Vec3f* v2, Vec3f* result) {
 /**
  * Normalizes the vector and returns its length.
  */
-f32 vec3_normalize(Vec3f* v) {
+f32 vec3_normalize(Vec3f *v) {
     f32 length;
     f32 lengthInv;
 
@@ -63,7 +63,7 @@ f32 vec3_normalize(Vec3f* v) {
 /**
  * Subtracts v2 from v1 and stores the difference in result.
  */
-void vec3_sub(Vec3f* v1, Vec3f* v2, Vec3f* result) {
+void vec3_sub(Vec3f *v1, Vec3f *v2, Vec3f *result) {
     result->x = v1->x - v2->x;
     result->y = v1->y - v2->y;
     result->z = v1->z - v2->z;
@@ -74,7 +74,7 @@ void vec3_sub(Vec3f* v1, Vec3f* v2, Vec3f* result) {
  * 
  * result = v1 + (scale * v2)
  */
-void vec3_add_with_scale(Vec3f* v1, Vec3f* v2, f32 scale, Vec3f* result) {
+void vec3_add_with_scale(Vec3f *v1, Vec3f *v2, f32 scale, Vec3f *result) {
     result->x = v1->x + scale * v2->x;
     result->y = v1->y + scale * v2->y;
     result->z = v1->z + scale * v2->z;
@@ -85,7 +85,7 @@ void vec3_add_with_scale(Vec3f* v1, Vec3f* v2, f32 scale, Vec3f* result) {
  * 
  * If the dot product of v1 and v2 is greater than 0, result will be set to v2.
  */
-void vec3_reflect(Vec3f* v1, Vec3f* v2, Vec3f* result) {
+void vec3_reflect(Vec3f *v1, Vec3f *v2, Vec3f *result) {
     // Calculate the angle (dot product) between v1 and v2
     float dot = v2->z * v1->z + (v1->x * v2->x + v1->y * v2->y);
 
