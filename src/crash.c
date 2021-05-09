c5e66b4a5904a2c888bf58786ff6451b858ec7a7
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2021-05-09 16:59:15 -0400

    Nearly match crash_thread_entry

--- src/crash.c
+++ src/crash.c
@@ -1,21 +1,38 @@
 #include <PR/os_internal.h>
 #include "common.h"
 #include "crash.h"
+#include "exception.h"
+#include "video.h"
+
+// Length of gCrashMesgQueueBuffer
+#define CRASH_MESG_QUEUE_BUFFER_LENGTH 1
 
 typedef struct _UnkCrashStruct {
-    u8 unk0x0[8];
-    u8 unk0x8;
+    /*0x0*/  DLLInst* loadedDllList;
+    /*0x4*/  s32 loadedDllCount;
+    /*0x8*/  u8 unk0x8;
 } UnkCrashStruct;
 
 extern UnkCrashStruct *D_80091770;
+extern OSThread *D_80091774[2];
 
 // Note: Unsure of actual stack size
 extern u8 gCrashThreadStack[OS_MIN_STACKSIZE];
 extern OSThread gCrashThread;
 
-void func_8005D410(s32 videoMode, OSSched* scheduler, s32 someBool);
+extern int D_800B3748;
+
+extern OSMesg gCrashMesgQueueBuffer[1];
+extern OSMesgQueue gCrashMesgQueue;
+
+extern s16 D_800B3770;
+
+OSSched *get_ossched(void);
+void func_8003B6E0(OSSched *scheduler, int*, OSMesgQueue*, int);
 
 void crash_thread_entry(void *arg);
+void stop_active_app_threads_2();
+void func_80037678();
 
 #if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/crash/start_crash_thread.s")
@@ -42,7 +59,60 @@ void start_crash_thread(OSSched* scheduler) {
 }
 #endif
 
+#if 1
 #pragma GLOBAL_ASM("asm/nonmatchings/crash/crash_thread_entry.s")
+#else
+// Functionally equivalent, mainly regalloc
+void crash_thread_entry(void *_) {
+    s16 *queueMesg;
+    OSSched *scheduler;
+    OSThread *stack0x20[2];
+
+    scheduler = get_ossched();
+    queueMesg = NULL;
+
+    stack0x20[0] = D_80091774[0];
+    stack0x20[1] = D_80091774[1];
+
+    osCreateMesgQueue(
+        &gCrashMesgQueue, 
+        &gCrashMesgQueueBuffer[0], 
+        CRASH_MESG_QUEUE_BUFFER_LENGTH
+    );
+
+    func_8003B6E0(scheduler, &D_800B3748, &gCrashMesgQueue, 3);
+
+    if (osResetType == 1 && D_80091770->unk0x8 == 1) {
+        D_800B3770 = 5;
+
+        osSendMesg(&gCrashMesgQueue, &D_800B3770, OS_MESG_NOBLOCK);
+    }
+
+    D_80091770->unk0x8 = 0;
+
+    osRecvMesg(&gCrashMesgQueue, (OSMesg)&queueMesg, OS_MESG_BLOCK);
+
+    if (*queueMesg == 4) {
+        stop_active_app_threads_2();
+        func_80037678();
+
+        // Halt
+        while (TRUE) { }
+    }
+
+    osViBlack(FALSE);
+    osViSwapBuffer(gFramebufferCurrent);
+
+    stop_active_app_threads();
+    check_video_mode_crash_and_clear_framebuffer();
+
+    some_crash_setter(D_80091770->loadedDllList, D_80091770->loadedDllCount);
+    some_crash_print(&stack0x20[0], 2, 0);
+
+    // Halt
+    while (TRUE) { }
+}
+#endif
 
 #if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/crash/stop_active_app_threads_2.s")
