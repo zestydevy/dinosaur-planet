d54f267ba92cd8cf97a6639c77b458d021255d60
zestydevy <zestydevy@gmail.com>
2021-07-28 22:37:28 -0500

    renaming files

---
+++ src/voxmaps.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/voxmaps/func_8000B8B0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/voxmaps/func_8000B8E4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/voxmaps/func_8000B970.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/voxmaps/func_8000BA20.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/voxmaps/func_8000BA50.s")
