7d1d95ab60c8f862cb224951eeddddd1ab5f961d
zestydevy <zestydevy@gmail.com>
2022-01-07 21:27:10 -0600

    func_8000B8B0

--- src/voxmaps.c
+++ src/voxmaps.c
@@ -1,6 +1,25 @@
 #include "common.h"
 
+
+// regalloc
+#if NON_MATCHING
+TMap *_func_8000B8B0(TMap *map, s32 arg1, s32 arg2, s32 arg3) {
+
+    s32 temp_t6 = arg2 >> 3;
+    map->m0004 = temp_t6;
+    if ((arg2 & 7) != 0) {
+        map->m0004 = temp_t6 + 1;
+    }
+    map->m0008 = arg2;
+    map->m000C = arg3;
+    map->m0000 = arg1;
+    map->m0010 = 0;
+
+    return map;
+}
+#else
 #pragma GLOBAL_ASM("asm/nonmatchings/voxmaps/func_8000B8B0.s")
+#endif
 
 #pragma GLOBAL_ASM("asm/nonmatchings/voxmaps/func_8000B8E4.s")
 
