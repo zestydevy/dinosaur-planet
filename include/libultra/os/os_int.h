20cfe8483c546cdf70c2f40ef5e0c25f482edb79
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-05 17:54:00 -0500

    Match osAiSetFrequency

---
+++ include/libultra/os/os_int.h
@@ -0,0 +1,8 @@
+#ifndef _LIBULTRA_OS_OS_INT_H
+#define _LIBULTRA_OS_OS_INT_H
+
+#include <PR/ultratypes.h>
+
+extern s32 osViClock;
+
+#endif //_LIBULTRA_OS_OS_INT_H
--- src/libultra/os/aisetfrequency.c
+++ src/libultra/os/aisetfrequency.c
@@ -1,3 +1,19 @@
-#include "common.h"
+#include <PR/rcp.h>
+#include "libultra/os/os_int.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/aisetfrequency/osAiSetFrequency.s")
+s32 osAiSetFrequency(u32 frequency)
+{
+    register unsigned int dacRate;
+    register char bitRate;
+    register float f;
+    f = osViClock / (float)frequency + .5f;
+    dacRate = f;
+    if (dacRate < AI_MIN_DAC_RATE)
+        return -1;
+    bitRate = dacRate / 66;
+    if (bitRate > AI_MAX_BIT_RATE)
+        bitRate = AI_MAX_BIT_RATE;
+    IO_WRITE(AI_DACRATE_REG, dacRate - 1);
+    IO_WRITE(AI_BITRATE_REG, bitRate - 1);
+    return osViClock / (s32)dacRate;
+}
