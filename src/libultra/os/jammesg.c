b2cc06402aef8cc0571ea8e27f30e0cf4bfbaa87
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-06 16:55:53 -0500

    Match __osAiDeviceBusy, osJamMesg, and osPiGetCmdQueue

--- src/libultra/os/jammesg.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/jammesg/__osAiDeviceBusy.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/jammesg/osJamMesg.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/jammesg/osPiGetCmdQueue.s")
