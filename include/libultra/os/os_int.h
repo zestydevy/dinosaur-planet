5a20f176193362ecbbed6fa3615d1c2bc86c4acc
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-06 13:16:12 -0500

    Match osCreateMesgQueue

--- include/libultra/os/os_int.h
+++ include/libultra/os/os_int.h
@@ -2,9 +2,49 @@
 #define _LIBULTRA_OS_OS_INT_H
 
 #include <PR/ultratypes.h>
+#include <PR/os_internal.h>
+
+typedef struct __OSEventState
+{
+    OSMesgQueue *messageQueue;
+    OSMesg message;
+} __OSEventState;
+
+extern struct __osThreadTail
+{
+    OSThread *next;
+    OSPri priority;
+} __osThreadTail;
+
+extern void __osEnqueueAndYield(OSThread **);
+extern void __osDequeueThread(OSThread **, OSThread *);
+extern void __osEnqueueThread(OSThread **, OSThread *);
+extern OSThread *__osPopThread(OSThread **);
+extern void __osDispatchThread(void);
+
+extern void __osSetTimerIntr(OSTime);
+extern OSTime __osInsertTimer(OSTimer *);
+extern void __osTimerInterrupt(void);
+extern u32 __osProbeTLB(void *);
+extern int     __osSpDeviceBusy(void);
+
+extern OSThread *__osRunningThread;
+extern OSThread *__osActiveQueue;
+extern OSThread *__osFaultedThread;
+extern OSThread *__osRunQueue;
+
+extern OSTimer *__osTimerList;
+extern OSTimer __osBaseTimer;
+extern OSTime __osCurrentTime;
+extern u32 __osBaseCounter;
+extern u32 __osViIntrCount;
+extern u32 __osTimerCounter;
+
+extern __OSEventState __osEventStateTab[OS_NUM_EVENTS];
 
 extern s32 osViClock;
-
+extern void __osTimerServicesInit(void);
 extern s32 __osAiDeviceBusy(void);
+extern int __osDpDeviceBusy(void);
 
 #endif //_LIBULTRA_OS_OS_INT_H
---
+++ src/libultra/os/O1/createmesgqueue.c
@@ -0,0 +1,12 @@
+#include <PR/os_internal.h>
+#include "libultra/os/os_int.h"
+
+void osCreateMesgQueue(OSMesgQueue *mq, OSMesg *msg, s32 msgCount)
+{
+    mq->mtqueue = (OSThread *)&__osThreadTail;
+    mq->fullqueue = (OSThread *)&__osThreadTail;
+    mq->validCount = 0;
+    mq->first = 0;
+    mq->msgCount = msgCount;
+    mq->msg = msg;
+}
