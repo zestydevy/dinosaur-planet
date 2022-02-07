38be19ee125954741e1afd27d286d6df6e4a8a70
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-06 23:03:55 -0500

    Match osSpTaskYielded

--- src/libultra/os/sptaskyielded.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/sptaskyielded/osSpTaskYielded.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/sptaskyielded/osViGetCurrentFramebuffer.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/sptaskyielded/osViGetNextFramebuffer.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/sptaskyielded/osDpSetNextBuffer.s")
---
+++ src/libultra/os/vigetcurrframebuf.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/vigetcurrframebuf/osViGetCurrentFramebuffer.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/vigetcurrframebuf/osViGetNextFramebuffer.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/vigetcurrframebuf/osDpSetNextBuffer.s")
