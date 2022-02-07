b62ae44af8c60fdbc79df75730e4cea2136b8aac
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-06 20:43:50 -0500

    Match __osSiCreateAccessQueue, __osSiGetAccess, and __osSiRelAccess

--- src/libultra/os/siacs.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/siacs/__osSiCreateAccessQueue.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/siacs/__osSiGetAccess.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/siacs/__osSiRelAccess.s")
