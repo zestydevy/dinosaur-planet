16042f0bc5b9c9756a1e1e6090e778f9d6bd068d
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-06 23:48:33 -0500

    Match osDpSetNextBuffer, __osViGetCurrentContext, osViGetCurrentFramebuffer, and osViGetNextFramebuffer

--- src/libultra/os/vigetcurrframebuf.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/vigetcurrframebuf/osViGetCurrentFramebuffer.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/vigetcurrframebuf/osViGetNextFramebuffer.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/vigetcurrframebuf/osDpSetNextBuffer.s")
