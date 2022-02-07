6099f9df34ebc2ce2b6058844835c86c98ae4687
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-06 23:20:12 -0500

    Match __osTimerServicesInit, __osSetTimerIntr, and __osInsertTimer

--- src/libultra/os/timerintr.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/timerintr/__osTimerServicesInit.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/timerintr/__osTimerInterrupt.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/timerintr/__osSetTimerIntr.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/os/timerintr/__osInsertTimer.s")
