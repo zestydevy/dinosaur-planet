f4e453bba83f91d1186a70dfb62f09c6b080ba6d
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-08 00:53:18 -0500

    Organize files for __osContRamWrite, osEPiRawReadIo, osEPiRawWriteIo, and _Printf

--- src/segment_83D80.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_83D80/osEPiRawWriteIo.s")
--- src/segment_84D30.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_84D30/__osContRamWrite.s")
