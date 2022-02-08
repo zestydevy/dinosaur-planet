09645344a6efa9ca172ee16c4fe4cbb46cd82b68
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-08 01:03:23 -0500

    Match __osDpDeviceBusy and __osSpGetStatus

---
+++ src/libultra/io/O1/spgetstat.c
@@ -0,0 +1,7 @@
+#include <PR/os_internal.h>
+#include <PR/rcp.h>
+
+u32 __osSpGetStatus()
+{
+    return IO_READ(SP_STATUS_REG);
+}
--- src/segment_83690.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_83690/__osDpDeviceBusy.s")
