fb8e4d7dd15a3a86642f6c460b422f6eeb76fa03
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-08 00:00:56 -0500

    Match func_8007CC9C, osStartThread, and osSetThreadPri

--- src/segment_7D410.c
+++ src/segment_7D410.c
@@ -2,10 +2,6 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_7D410/osInitialize.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_7D410/func_8007CC9C.s")
+void func_8007CC9C() {}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_7D410/__createSpeedParam.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_7D410/osStartThread.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_7D410/osSetThreadPri.s")
