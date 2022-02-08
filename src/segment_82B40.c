cae921da76c192bc29421dbfe2e40f5cbbafadc9
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-08 00:20:42 -0500

    Match osPiRawStartDma, __osPiCreateAccessQueue, __osPiGetAccess, and __osPiRelAccess

--- src/segment_82B40.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_82B40/__osPiCreateAccessQueue.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_82B40/__osPiGetAccess.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_82B40/__osPiRelAccess.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_82B40/osPiRawStartDma.s")
