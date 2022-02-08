1070c033ae4130af71f4d82fa6e0d2393acd2134
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-07 21:47:47 -0500

    Match memcpy, strlen, strchr

--- src/libultra/libc/string.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/libc/string/memcpy.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/libc/string/strlen.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/libc/string/strchr.s")
