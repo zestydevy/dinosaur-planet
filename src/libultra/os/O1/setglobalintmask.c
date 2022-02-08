eff52ec196817259d18e4744a618cd8dfa02dab1
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-02-08 00:34:02 -0500

    Match __osResetGlobalIntMask and __osResetGlobalIntMask

---
+++ src/libultra/os/O1/setglobalintmask.c
@@ -0,0 +1,9 @@
+#include <PR/os_internal.h>
+#include <PR/R4300.h>
+
+void __osSetGlobalIntMask(OSHWIntr mask)
+{
+    register u32 saveMask = __osDisableInt();
+    __OSGlobalIntMask |= mask;
+    __osRestoreInt(saveMask);
+}
--- src/segment_85550.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_85550/__osSetGlobalIntMask.s")
