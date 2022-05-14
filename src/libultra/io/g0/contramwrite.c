d7199418e1da61db3bafa4b4478ae4ab2a8977eb
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-05-14 02:56:32 -0400

    Match __osContRamWrite

--- src/libultra/io/g0/contramwrite.c
+++ src/libultra/io/g0/contramwrite.c
@@ -3,16 +3,22 @@
 #include <libultra/io/controller.h>
 #include <libultra/io/siint.h>
 
-int __osPfsLastChannel = -1;
+extern int __osPfsLastChannel;
 
-s32 __osContRamRead(OSMesgQueue *mq, int channel, u16 address, u8 *buffer) {
+s32 __osContRamWrite(OSMesgQueue *mq, int channel, u16 address, u8 *buffer, int force) {
     s32 ret;
     int i;
     __OSContRamReadFormat *pifPtr;
     s32 retry;
+    u8 crc;
 
-    // make 3 attempts at a read
+    // make 3 attempts at a write
     retry = 2;
+
+    // skip if not forcing and address is [1,6] (protected region?)
+    if ((force != 1) && (address < 7) && (address != 0)) {
+        return 0;
+    }
 
     // acquire lock for SI access
     __osSiGetAccess();
@@ -21,9 +27,9 @@ s32 __osContRamRead(OSMesgQueue *mq, int channel, u16 address, u8 *buffer) {
         // get pointer into PIF ram
         pifPtr = (__OSContRamReadFormat *)&__osPfsPifRam;
 
-        // skip format update if the previous command was a read on the same channel
-        if ((__osContLastCmd != CONT_RAM_READ) || (channel != __osPfsLastChannel)) {
-            __osContLastCmd = CONT_RAM_READ;
+        // skip format update if the previous command was a write on the same channel
+        if ((__osContLastCmd != CONT_RAM_WRITE) || (channel != __osPfsLastChannel)) {
+            __osContLastCmd = CONT_RAM_WRITE;
             __osPfsLastChannel = channel;
 
             // ???
@@ -33,9 +39,9 @@ s32 __osContRamRead(OSMesgQueue *mq, int channel, u16 address, u8 *buffer) {
             __osPfsPifRam.pifstatus = CONT_FORMAT;
 
             pifPtr->dummy = 0xFF;
-            pifPtr->txsize = 3;
-            pifPtr->rxsize = 0x21;
-            pifPtr->cmd = CONT_RAM_READ;
+            pifPtr->txsize = 0x23;
+            pifPtr->rxsize = 1;
+            pifPtr->cmd = CONT_RAM_WRITE;
             pifPtr->datacrc = 0xFF;
 
             ((u8*)pifPtr)[sizeof(__OSContRamReadFormat)] = FORMAT_END;
@@ -47,9 +53,13 @@ s32 __osContRamRead(OSMesgQueue *mq, int channel, u16 address, u8 *buffer) {
         // ???
         pifPtr->address_hi = (u8)(address >> 3);
         pifPtr->address_lo = (u8)(__osContAddressCrc(address) | (address << 5));
+
+        // copy given data to PIF data
+        bcopy(buffer, pifPtr->data, BLOCKSIZE);
         
         // DMA PIF data
         __osSiRawStartDma(OS_WRITE, &__osPfsPifRam);
+        crc = __osContDataCrc(buffer); // calculate CRC of sent data
         osRecvMesg(mq, (OSMesg *)NULL, OS_MESG_BLOCK);
 
         __osSiRawStartDma(OS_READ, &__osPfsPifRam);
@@ -60,7 +70,7 @@ s32 __osContRamRead(OSMesgQueue *mq, int channel, u16 address, u8 *buffer) {
         
         if (ret == 0) {
             // validate CRC
-            if (pifPtr->datacrc != __osContDataCrc(pifPtr->data)) {
+            if (crc != pifPtr->datacrc) {
                 // CRC failed, retry if status is 0
                 ret = __osPfsGetStatus(mq, channel);
                 if (ret != 0) {
@@ -68,9 +78,6 @@ s32 __osContRamRead(OSMesgQueue *mq, int channel, u16 address, u8 *buffer) {
                 } else {
                     ret = PFS_ERR_CONTRFAIL;
                 }
-            } else {
-                // success, copy data to given buffer
-                bcopy(pifPtr->data, buffer, BLOCKSIZE);
             }
         } else {
             // failed
