2f568e5650bc10ae80ee894c111ee8672e1b8505
Chris Richardson <chris.richardson1801@gmail.com>
2023-12-20 01:42:47 +1030

    did one funciton, begun another

--- src/segment_89080.c
+++ src/segment_89080.c
@@ -12,7 +12,29 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/__rmonGetBranchTarget.s")
 
+#if 1
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/func_80088B64.s")
+#else
+    /* uses O1 */
+    int func_80088B64(u32 arg0) {
+    
+    switch ((arg0 >> 0x1A) & 0x3F) {                              /* irregular */
+        case 0:
+            if (!((arg0 >> 5) & 0x7FFF) && ((arg0 & 0x3F) == 8)) {
+                return 1;
+            }
+            if (!((arg0 >> 0x10) & 0x1F) && ((arg0 & 0x7FF) == 9)) {
+                return 1;
+            }
+        break;
+        case 2:
+        case 3:
+            return 1;
+        }
+        
+    return 0;
+    }
+#endif
 
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/__rmonSetSingleStep.s")
 
