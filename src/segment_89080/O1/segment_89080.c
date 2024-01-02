867ccffd6e369029c5bfb93c5f67af4bd35e7170
Chris Richardson <chris.richardson1801@gmail.com>
2024-01-02 16:09:36 +1030

    did a few functions in segment_89080

--- src/segment_89080/O1/segment_89080.c
+++ src/segment_89080/O1/segment_89080.c
@@ -1,12 +1,77 @@
 #include "common.h"
 
+/* Declaring func_80088E80 up here, as other funcs use it before its implementation which causes redeclaration compile errors*/
+void func_80088E80();
+
+
+#if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/O1/segment_89080/func_80088480.s")
+#else
+extern s32* D_800D3E60;
+extern s32 D_800D3E64;
+extern s32* rmonbrk_bss_0000[4]; /* size may be wrong */
 
+void func_80088480(s32* arg0, s32* arg1) {
+    rmonbrk_bss_0000[1] = (s32) *arg0;
+    *arg0 = 0x40D;
+    osWritebackDCache(arg0, 4);
+    osInvalICache(arg0, 4);
+    rmonbrk_bss_0000[0] = arg0;
+    if (arg1 != NULL) {
+        D_800D3E64 = *arg1;
+        *arg1 = 0x40D;
+        osWritebackDCache(arg1, 4);
+        osInvalICache(arg1, 4);
+        D_800D3E60 = arg1;
+    }
+}
+#endif
+
+#if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/O1/segment_89080/func_8008852C.s")
+#else
+extern s32* D_800D3E60;
+extern s32 D_800D3E64;
+extern s32* rmonbrk_bss_0000[4]; /* size may be wrong */
 
+void func_8008852C(void) {
+    s32 sp1C;
+
+    if (rmonbrk_bss_0000[0] != NULL) {
+        sp1C = *rmonbrk_bss_0000[0];
+        if ((sp1C & 0xFC00003F) == 0xD) {
+            *rmonbrk_bss_0000[0] = rmonbrk_bss_0000[1];
+            osWritebackDCache(rmonbrk_bss_0000[0], 4);
+            osInvalICache(rmonbrk_bss_0000[0], 4);
+        }
+        rmonbrk_bss_0000[0] = NULL;
+    }
+    if (D_800D3E60 != NULL) {
+        sp1C = *D_800D3E60;
+        if ((sp1C & 0xFC00003F) == 0xD) {
+            *D_800D3E60 = D_800D3E64;
+            osWritebackDCache(D_800D3E60, 4);
+            osInvalICache(D_800D3E60, 4);
+        }
+        D_800D3E60 = NULL;
+    }
+}
+#endif
+
+#if 1
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/O1/segment_89080/__rmonSetBreak.s")
+#else
 
+#endif
+
+#if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/O1/segment_89080/func_800887D4.s")
+#else
+/* Must be unused*/
+int func_800887D4(s32 arg0) {
+    return -1;
+}
+#endif
 
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/O1/segment_89080/__rmonClearBreak.s")
 
@@ -36,7 +101,32 @@
 
 #endif
 
+#if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/O1/segment_89080/__rmonSetSingleStep.s")
+#else
+int __rmonSetSingleStep(s32 arg0, s32* arg1) {
+    s32 sp1C;
+
+    sp1C = __rmonGetBranchTarget(0, arg0, arg1);
+    if (sp1C & 3) {
+        /* changed the + 4 to + 1, compiler seems to convert appropratly (pretty sure it means 1 word or byte) */
+        func_80088480((s32) (arg1 + 1), NULL);
+        
+    }else if ((arg1+0) == (void*)sp1C) { /* had to add the + 0 to arg1 to get function to match */
+        return 0;
+    }
+    /* changed the + 8 to + 2, compiler seems to convert appropratly (pretty sure it means 2 words or bytes) */
+    else if ((func_80088B64(*arg1) != 0) || ((arg1 + 2) == (void*)sp1C)) { 
+        func_80088480(sp1C, NULL);
+    } else {
+        /* Same here*/
+        func_80088480(sp1C, arg1 + 2);
+    }
+    
+    
+    return 1;
+}
+#endif
 
 #if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/O1/segment_89080/__rmonGetExceptionStatus.s")
@@ -104,9 +194,13 @@ void func_80088CFC(s32* arg0, s32 arg1) {
 }
 #endif
 
+
+
+
 #if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/O1/segment_89080/__rmonHitBreak.s")
 #else
+
 void __rmonHitBreak(void) {
     func_8008852C();
     __rmonStopUserThreads(0);
@@ -136,15 +230,40 @@ void __rmonHitSpBreak(void) {
 #if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/O1/segment_89080/func_80088E50.s")
 #else
-    void func_80088E50(void) {
+void func_80088E50(void) {
         __rmonMaskIdleThreadInts();
         __rmonStopUserThreads(0);
         func_80088E80();
     }
 #endif
 
-#if 1
+#if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_89080/O1/segment_89080/func_80088E80.s")
 #else
+void func_80088E80() {
 
+    /*Seems right to use register keyword, got compiler to use 'save' register*/
+    register struct OSThread * var_s0 = __osGetActiveQueue();
+    s32 sp20;
+
+
+    while (var_s0->priority != -1){
+        if ((var_s0->priority > 0) && (var_s0->priority < 0x80)) {
+            if (var_s0->flags & 1) {
+                sp20 = *(var_s0->context.pc);
+                if ((sp20 & 0xFC00003F) == 0xD) {
+                    func_80088CFC(var_s0->id, sp20 >> 6);
+                } else {
+                    func_80088CFC(var_s0->id, 0);
+                }
+            }
+            if (var_s0->flags & 2) {
+                __rmonSendFault(var_s0);
+                func_80088CFC(var_s0->id, 0xF);
+            }
+        }
+        var_s0 = var_s0->tlnext;/*gives warning but should be fine*/
+    } 
+    
+}
 #endif
