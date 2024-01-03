5bebc521db9e5704e5730e1b47111f37caa11122
Chris Richardson <chris.richardson1801@gmail.com>
2024-01-03 13:11:22 +1030

    did segment_5FFB0, which seems like skeleton code

--- src/segment_5FFB0.c
+++ src/segment_5FFB0.c
@@ -1,5 +1,19 @@
 #include "common.h"
 
+#if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_5FFB0/func_8005F3B0.s")
+#else
+/* Must be unfinished, assumed arg1 and arg2 types*/
+void func_8005F3B0(s32 arg0, s32 arg1, s32 arg2) {
+    return;
+}
+#endif
 
+#if 0
 #pragma GLOBAL_ASM("asm/nonmatchings/segment_5FFB0/func_8005F3C4.s")
+#else
+/* Must be unfinished, assumed arg1 and arg2 types*/
+void func_8005F3C4(s32 arg0, s32 arg1, s32 arg2) {
+    return;
+}
+#endif
