68b71be4e5565d8e2aad24cef911a53505215bf5
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-03-01 17:14:25 -0500

    Match _Litob

--- src/libultra/libc/xlitob.c
+++ src/libultra/libc/xlitob.c
@@ -1,3 +1,54 @@
-#include "common.h"
+// @DECOMP_OPT_FLAGS=-O2 -g0
+#include "libc/stdlib.h"
+#include "libc/string.h"
+#include "libc/xstdio.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/libc/xlitob/_Litob.s")
+#define BUFF_LEN 0x18
+
+static char ldigs[] = "0123456789abcdef";
+static char udigs[] = "0123456789ABCDEF";
+
+void _Litob(_Pft *args, char type) {
+    char buff[BUFF_LEN];
+    const char *digs;
+    s32 base;
+    s32 i;
+    unsigned long long ullval;
+
+    digs = (type == 'X') ? udigs : ldigs;
+
+    base = (type == 'o') ? 8 : ((type != 'x' && type != 'X') ? 10 : 16);
+    i = BUFF_LEN;
+    ullval = args->v.ll;
+
+    if ((type == 'd' || type == 'i') && args->v.ll < 0) {
+        ullval = -ullval;
+    }
+
+    if (ullval != 0 || args->prec != 0) {
+        buff[--i] = digs[ullval % base];
+    }
+
+    args->v.ll = ullval / base;
+
+    while (args->v.ll > 0 && i > 0) {
+        lldiv_t qr = lldiv(args->v.ll, base);
+        
+        args->v.ll = qr.quot;
+        buff[--i] = digs[qr.rem];
+    }
+
+    args->n1 = BUFF_LEN - i;
+
+    memcpy(args->s, buff + i, args->n1);
+
+    if (args->n1 < args->prec) {
+        args->nz0 = args->prec - args->n1;
+    }
+
+    if (args->prec < 0 && (args->flags & (FLAGS_ZERO | FLAGS_MINUS)) == FLAGS_ZERO) {
+        if ((i = args->width - args->n0 - args->nz0 - args->n1) > 0) {
+            args->nz0 += i;
+        }
+    }
+}
