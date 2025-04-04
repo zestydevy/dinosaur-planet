52933c05dfbd4b2402b21e03873f535db2390605
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-04-04 01:48:58 -0400

    Name assert.c and map .rodata

---
+++ src/assert.c
@@ -0,0 +1,16 @@
+#include "PR/ultratypes.h"
+
+// These strings are *most likely* for this file, but it's tough to confirm...
+// Neither of the functions are used.
+static const char str_8009ad90[] = "\nAssertion failed: '%s' in file %s, line %d\n";
+static const char str_8009adc0[] = "\nAssertion failed: '%s' in file %s, line %d\n";
+
+// maybe assert(const char *message, const char *file, s32 line) ?
+void func_8005F3B0(s32 arg0, s32 arg1, s32 arg2) {
+    return;
+}
+
+// maybe assert(const char *message, const char *file, s32 line) ?
+void func_8005F3C4(s32 arg0, s32 arg1, s32 arg2) {
+    return;
+}
