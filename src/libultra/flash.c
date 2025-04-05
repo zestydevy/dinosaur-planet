7bf8ac7c72aa0feb201b4d9b2511b18ad55034aa
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-04-04 20:11:58 -0400

    Match libultra flash functions (not the DLL ones)

---
+++ src/libultra/flash.c
@@ -0,0 +1,209 @@
+// @DECOMP_OPT_FLAGS=-O0 -g0
+// @DECOMP_MIPS_ISET=-mips1
+
+#include "PR/R4300.h"
+#include "PR/os_flash.h"
+#include "PR/os_internal_flash.h"
+#include "macros.h"
+
+OSPiHandle *osFlashReInit(u8 latency, u8 pulse, u8 page_size, u8 rel_duration, u32 start) {
+    __osFlashHandler.baseAddress = PHYS_TO_K1(start);
+    __osFlashHandler.type++;
+    __osFlashHandler.latency = latency;
+    __osFlashHandler.pulse = pulse;
+    __osFlashHandler.pageSize = page_size;
+    __osFlashHandler.relDuration = rel_duration;
+    __osFlashHandler.domain = PI_DOMAIN2;
+
+    return &__osFlashHandler;
+}
+
+OSPiHandle *osFlashInit() {
+    u32 flash_type;
+    u32 flash_maker;
+
+    osCreateMesgQueue(&__osFlashMessageQ, __osFlashMsgBuf, ARRAYCOUNT(__osFlashMsgBuf));
+
+    if (__osFlashHandler.baseAddress == PHYS_TO_K1(FLASH_START_ADDR)) {
+        return &__osFlashHandler;
+    }
+
+    __osFlashHandler.type = DEVICE_TYPE_FLASH;
+    __osFlashHandler.baseAddress = PHYS_TO_K1(FLASH_START_ADDR);
+    __osFlashHandler.latency = FLASH_LATENCY;
+    __osFlashHandler.pulse = FLASH_PULSE;
+    __osFlashHandler.pageSize = FLASH_PAGE_SIZE;
+    __osFlashHandler.relDuration = FLASH_REL_DURATION;
+    __osFlashHandler.domain = PI_DOMAIN2;
+    __osFlashHandler.speed = 0;
+
+    bzero(&__osFlashHandler.transferInfo, sizeof(__OSTranxInfo));
+
+    osEPiLinkHandle(&__osFlashHandler);
+    osFlashReadId(&flash_type, &flash_maker);
+
+    if (flash_maker == FLASH_VERSION_MX_C || flash_maker == FLASH_VERSION_MX_A ||
+        flash_maker == FLASH_VERSION_MX_PROTO_A) {
+        __osFlashVersion = 0x40;
+    } else {
+        __osFlashVersion = 0x80;
+    }
+
+    return &__osFlashHandler;
+}
+
+void osFlashReadStatus(u8* flash_status) {
+    u32 status;
+
+    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_STATUS);
+    // read status
+    osEPiReadIo(&__osFlashHandler, __osFlashHandler.baseAddress, &status);
+
+    // why twice ?
+    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_STATUS);
+    osEPiReadIo(&__osFlashHandler, __osFlashHandler.baseAddress, &status);
+
+    *flash_status = status & 0xFF;
+    return;
+}
+
+void osFlashReadId(u32* flash_type, u32* flash_maker) {
+    u8 tmp;
+
+    // why read status ?
+    osFlashReadStatus(&tmp);
+
+    // select silicon id read mode
+    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_ID);
+
+    // read silicon id using DMA
+    __osFlashMsg.hdr.pri = OS_MESG_PRI_NORMAL;
+    __osFlashMsg.hdr.retQueue = &__osFlashMessageQ;
+    __osFlashMsg.dramAddr = __osFlashID;
+    __osFlashMsg.devAddr = 0;
+    __osFlashMsg.size = 2 * sizeof(u32);
+
+    osWritebackDCache(__osFlashID, sizeof(__osFlashID));
+    osEPiStartDma(&__osFlashHandler, &__osFlashMsg, OS_READ);
+    osRecvMesg(&__osFlashMessageQ, NULL, OS_MESG_BLOCK);
+
+    *flash_type = __osFlashID[0];
+    *flash_maker = __osFlashID[1];
+    return;
+}
+
+void osFlashClearStatus(void) {
+    // select status mode
+    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_STATUS);
+    // clear status
+    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress, 0);
+    return;
+}
+
+s32 osFlashAllErase(void) {
+    u32 status;
+
+    // start chip erase operation
+    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_CHIP_ERASE);
+    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_EXECUTE_ERASE);
+
+    // wait for completion by polling erase-busy flag
+    do {
+        osEPiReadIo(&__osFlashHandler, __osFlashHandler.baseAddress, &status);
+    } while ((status & FLASH_STATUS_ERASE_BUSY) == FLASH_STATUS_ERASE_BUSY);
+
+    // check erase operation status, clear status
+    osEPiReadIo(&__osFlashHandler, __osFlashHandler.baseAddress, &status);
+    osFlashClearStatus();
+
+    // check for success
+    if (((status & 0xFF) == 8) || ((status & 0xFF) == 0x48) || ((status & 8) == 8)) {
+        return FLASH_STATUS_ERASE_OK;
+    } else {
+        return FLASH_STATUS_ERASE_ERROR;
+    }
+}
+
+s32 osFlashSectorErase(u32 page_num) {
+    u32 status;
+
+    // start sector erase operation
+    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_SECTOR_ERASE | page_num);
+    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_EXECUTE_ERASE);
+
+    // wait for completion by polling erase-busy flag
+    do {
+        osEPiReadIo(&__osFlashHandler, __osFlashHandler.baseAddress, &status);
+    } while ((status & FLASH_STATUS_ERASE_BUSY) == FLASH_STATUS_ERASE_BUSY);
+
+    // check erase operation status, clear status
+    osEPiReadIo(&__osFlashHandler, __osFlashHandler.baseAddress, &status);
+    osFlashClearStatus();
+
+    if (((status & 0xFF) == 8) || ((status & 0xFF) == 0x48) || ((status & 8) == 8)) {
+        return FLASH_STATUS_ERASE_OK;
+    } else {
+        return FLASH_STATUS_ERASE_ERROR;
+    }
+}
+
+s32 osFlashWriteBuffer(OSIoMesg* mb, s32 priority, void* dramAddr, OSMesgQueue* mq) {
+    s32 ret;
+
+    // select page program mode
+    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_PAGE_PROGRAM);
+
+    // DMA 128-byte page
+    mb->hdr.pri = priority;
+    mb->hdr.retQueue = mq;
+    mb->dramAddr = dramAddr;
+    mb->devAddr = 0;
+    mb->size = FLASH_BLOCK_SIZE;
+
+    ret = osEPiStartDma(&__osFlashHandler, mb, OS_WRITE);
+
+    return ret;
+}
+
+s32 osFlashWriteArray(u32 page_num) {
+    u32 status;
+
+    // start program page operation
+    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_PROGRAM_PAGE | page_num);
+
+    // wait for completion by polling write-busy flag
+    do {
+        osEPiReadIo(&__osFlashHandler, __osFlashHandler.baseAddress, &status);
+    } while ((status & FLASH_STATUS_WRITE_BUSY) == FLASH_STATUS_WRITE_BUSY);
+
+    // check program operation status, clear status
+    osEPiReadIo(&__osFlashHandler, __osFlashHandler.baseAddress, &status);
+    osFlashClearStatus();
+
+    if (((status & 0xFF) == 4) || ((status & 0xFF) == 0x44) || ((status & 4) == 4)) {
+        return FLASH_STATUS_WRITE_OK;
+    } else {
+        return FLASH_STATUS_WRITE_ERROR;
+    }
+}
+
+s32 osFlashReadArray(OSIoMesg* mb, s32 priority, u32 page_num, void* dramAddr, u32 n_pages, OSMesgQueue* mq) {
+    u32 ret;
+    u32 tmp;
+
+    // select read array mode
+    osEPiWriteIo(&__osFlashHandler, __osFlashHandler.baseAddress | FLASH_CMD_REG, FLASH_CMD_READ_ARRAY);
+    // dummy read?
+    osEPiReadIo(&__osFlashHandler, __osFlashHandler.baseAddress, &tmp);
+
+    // DMA requested pages
+    mb->hdr.pri = priority;
+    mb->hdr.retQueue = mq;
+    mb->dramAddr = dramAddr;
+    mb->devAddr = page_num * __osFlashVersion;
+    mb->size = n_pages * FLASH_BLOCK_SIZE;
+
+    ret = osEPiStartDma(&__osFlashHandler, mb, OS_READ);
+
+    return ret;
+}
