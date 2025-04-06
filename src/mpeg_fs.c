e998060e0ce2446008814ee2e064e63644b2e9c8
JackieLarue <jacqi.larue@gmail.com>
2025-04-06 01:53:46 -0500

    mpeg_fs.c decomp

--- src/mpeg_fs.c
+++ src/mpeg_fs.c
@@ -1,5 +1,20 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/mpeg_fs/mpeg_fs_init.s")
+extern s32* gFile_MPEG_TAB;
+void mp3_init(s32 arg0);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/mpeg_fs/mpeg_fs_play.s")
+void mpeg_fs_init(s32 arg0) {
+	queue_alloc_load_file((void**) (&gFile_MPEG_TAB), MPEG_TAB);
+	mp3_init(arg0);
+}
+
+void mpeg_fs_play(s32 id) {
+    s32 size;
+    s32 address;
+
+    address = file_get_romaddr(MPEG_BIN, gFile_MPEG_TAB[id]);
+    size = gFile_MPEG_TAB[id + 1] - gFile_MPEG_TAB[id];
+    if ((address != 0) && (size != 0)) {
+        mp3_play_file(address, size);
+    }
+}
