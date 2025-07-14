a78050990dd414c18109f54a256222a9cc6a8c22
Bl00D4NGEL <github@d-peters.com>
2025-07-14 19:22:58 +0200

    feat: add dll/23.h to expose dll 23 funcs

---
+++ include/dlls/engine/23.h
@@ -0,0 +1,18 @@
+#ifndef _DLLS_23_H
+#define _DLLS_23_H
+
+#include "PR/ultratypes.h"
+#include "dll_def.h"
+
+DLL_INTERFACE_BEGIN(23)
+/* 0 */ void (*dll_23_func_24)(s32 arg0);
+/* 1 */ void (*dll_23_func_30)(s32 arg0);
+/* 2 */ void (*dll_23_func_18)(s32 arg0);
+/* 3 */ s32 (*dll_23_func_3C)(s32 arg0);
+/* 4 */ void (*dll_23_func_4C)(s32 arg0, f32 arg1, f32 arg2);
+/* 5 */ void (*dll_23_func_60)(s32 arg0);
+/* 6 */ void (*dll_23_func_6C)(void);
+/* 7 */ void (*dll_23_func_8C)(s32 arg0);
+DLL_INTERFACE_END()
+
+#endif //_DLLS_23_H
