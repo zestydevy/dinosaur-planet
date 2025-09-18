526aec78342c40ada0dee17a0436c3bb20266dc4
Bl00D4NGEL <github@d-peters.com>
2025-09-18 22:05:14 +0200

    match: segment_1050.c

--- include/dlls/engine/57.h
+++ include/dlls/engine/57.h
@@ -7,7 +7,7 @@
 DLL_INTERFACE(DLL_57) {
 /*:*/ DLL_INTERFACE_BASE(DLL);
 /*0*/ void (*func0)(void);
-/*1*/ UnknownDLLFunc func1;
+/*1*/ void (*func1)(void *);
 /*2*/ void (*func2)(s32 *, s32 *, s32 *, s32 *, s32 *, s32 *);
 /*3*/ void (*func3)(void);
 /*4*/ UnknownDLLFunc func4;
--- src/segment_1050.c
+++ src/segment_1050.c
@@ -1,5 +1,61 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_1050/func_80000450.s")
+s32 func_80000450(Object* arg0, Object* arg1, u16 arg2, s8 arg3, s32 arg4, s32 arg5) {
+    NewLfxStruct* temp_v0;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/segment_1050/func_80000608.s")
+    temp_v0 = mmAlloc(0x28, -1, 0);
+    queue_load_file_region_to_ptr((void *)temp_v0, 0xC, arg2 * 0x28, 0x28);
+    if (temp_v0 != NULL) {
+        if (arg5 != 0) {
+            temp_v0->unk12.asByte |= 0x40;
+            if (temp_v0->unk1b == 2) {
+                gDLL_57->vtbl->func1(temp_v0);
+            } else if (temp_v0->unk1b == 5) {
+                gDLL_58->vtbl->func1(temp_v0);
+            } else {
+                gDLL_11_Newlfx->vtbl->func0(NULL, arg1, temp_v0, arg3, arg4, arg5);
+            }
+        } else {
+            if (temp_v0->unk1b == 2) {
+                gDLL_57->vtbl->func1(temp_v0);
+            } else if (temp_v0->unk1b == 5) {
+                gDLL_58->vtbl->func1(temp_v0);
+            } else {
+                gDLL_11_Newlfx->vtbl->func0(arg0, arg1, temp_v0, arg3, arg4, 0);
+            }
+        }
+    }
+    mmFree(temp_v0);
+    return 0;
+}
+
+s32 func_80000608(Object* arg0, Object* arg1, u16 arg2, s8 arg3, s32 arg4, s32 arg5) {
+    NewLfxStruct* temp_v0;
+
+    temp_v0 = mmAlloc(0x28, -1, NULL);
+    queue_load_file_region_to_ptr((void *)temp_v0, 0xC, arg2 * 0x28, 0x28);
+    if (temp_v0 != NULL) {
+        temp_v0->unk0 = 0xC;
+        if (arg5 != 0) {
+            temp_v0->unk12.asByte |= 0x40;
+            if (temp_v0->unk1b == 2) {
+                gDLL_57->vtbl->func1(temp_v0);
+            } else if (temp_v0->unk1b == 5) {
+                gDLL_58->vtbl->func1(temp_v0);
+            } else {
+                gDLL_11_Newlfx->vtbl->func0(NULL, arg1, temp_v0, arg3, arg4, arg5);
+            }
+        } else {
+            if (temp_v0->unk1b == 2) {
+                gDLL_57->vtbl->func1(temp_v0);
+            } else if (temp_v0->unk1b == 5) {
+                gDLL_58->vtbl->func1(temp_v0);
+            } else {
+                gDLL_11_Newlfx->vtbl->func0(arg0, arg1, temp_v0, arg3, arg4, 0);
+            }
+        }
+    }
+    mmFree(temp_v0);
+    return 0;
+}
+
