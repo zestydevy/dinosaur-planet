34e8a9ff76b1aa81ad8f0f4384b2d1cb503fbb43
Banjeoin <e.odrisceoil@gmail.com>
2025-09-25 22:32:37 +0100

    Function arguments for DLL_27_head_turn

--- include/dlls/engine/27_head_turn.h
+++ include/dlls/engine/27_head_turn.h
@@ -13,9 +13,9 @@ DLL_INTERFACE(DLL_27_head_turn) {
     /*0*/ void (*head_turn_func_18)(Vec3f*, s32, s32, s32);
     /*1*/ UnknownDLLFunc head_turn_func_84;
     /*2*/ void (*head_turn_func_c0)(Vec3f*, s32, s32, s32, s32);
-    /*3*/ void (*head_turn_func_1e8)(Object* object, Vec3f* vector, f32 delayFloat);
-    /*4*/ void (*head_turn_func_5a8)(Object* object, Vec3f* vector);
-    /*5*/ void (*head_turn_func_624)(Object* object, Vec3f* vector, f32 delayFloat);
+    /*3*/ void (*head_turn_func_1e8)(Object* object, Vec3f** vector, f32 delayFloat);
+    /*4*/ void (*head_turn_func_5a8)(Object* object, Vec3f** vector);
+    /*5*/ void (*head_turn_func_624)(Object* object, Vec3f** vector, f32 delayFloat);
     /*6*/ UnknownDLLFunc head_turn_func_c7c;
     /*7*/ void (*head_turn_func_fb8)(Object* object, void*);
     /*8*/ UnknownDLLFunc head_turn_func_df4;
