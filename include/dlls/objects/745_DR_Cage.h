bfac5c4b69935c279490366244b8001691825a27
Banjeoin <e.odrisceoil@gmail.com>
2025-10-08 17:24:31 +0100

    DR_Cage DLL header

---
+++ include/dlls/objects/745_DR_Cage.h
@@ -0,0 +1,21 @@
+#ifndef _DLLS_745_H
+#define _DLLS_745_H
+
+#include "PR/ultratypes.h"
+#include "game/objects/object.h"
+#include "dll_def.h"
+#include "types.h"
+
+DLL_INTERFACE(DLL_745_DR_Cage) {
+    /*:*/ DLL_INTERFACE_BASE(DLL_IObject);
+    /*0*/ s32 (*func0)(Object*);
+    /*1*/ UnknownDLLFunc func1;
+    /*2*/ UnknownDLLFunc func2;
+    /*3*/ UnknownDLLFunc func3;
+    /*4*/ UnknownDLLFunc func4;
+    /*5*/ UnknownDLLFunc func5;
+    /*6*/ UnknownDLLFunc func6;
+    /*7*/ UnknownDLLFunc func7;
+};
+
+#endif //_DLLS_227_H
