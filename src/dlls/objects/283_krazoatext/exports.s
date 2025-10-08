d06e3024c023e87d0b29d78df5ba7d7340bd87c9
Banjeoin <e.odrisceoil@gmail.com>
2025-10-08 17:55:43 +0100

    krazoatext symbols

--- src/dlls/objects/283_krazoatext/exports.s
+++ src/dlls/objects/283_krazoatext/exports.s
@@ -4,15 +4,15 @@
 _exports:
 
 # ctor/dtor
-.dword dll_283_ctor
-.dword dll_283_dtor
+.dword krazoatext_ctor
+.dword krazoatext_dtor
 
 # export table
-/*0*/ .dword dll_283_create
-/*1*/ .dword dll_283_update
-/*2*/ .dword dll_283_func_324
-/*3*/ .dword dll_283_draw
-/*4*/ .dword dll_283_destroy
-/*5*/ .dword dll_283_get_setup_flags
-/*6*/ .dword dll_283_get_state_size
-/*7*/ .dword dll_283_func_438
+.dword krazoatext_create
+.dword krazoatext_update
+.dword krazoatext_func_324
+.dword krazoatext_draw
+.dword krazoatext_destroy
+.dword krazoatext_func_414
+.dword krazoatext_get_state_size
+.dword krazoatext_print
