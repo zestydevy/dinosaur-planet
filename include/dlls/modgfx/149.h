21ec91e74ecd94e8f17ab5ed5f1105005970fa94
Souris-2d07 <236702845+Decomp123@users.noreply.github.com>
2025-10-13 11:02:17 +1030

    Match 610_WL_spiritplace (#142)
    * Match 610_WL_spiritplace
    
    * revert DLL_ID_MODGFX

---
+++ include/dlls/modgfx/149.h
@@ -0,0 +1,10 @@
+#ifndef _DLLS_149_H
+#define _DLLS_149_H
+
+#include "sys/gfx/modgfx.h"
+
+DLL_INTERFACE(DLL_149) {
+    /*:*/ DLL_INTERFACE_BASE(DLL_IModgfx);
+};
+
+#endif //_DLLS_149_H
---
+++ include/dlls/modgfx/155.h
@@ -0,0 +1,10 @@
+#ifndef _DLLS_155_H
+#define _DLLS_155_H
+
+#include "sys/gfx/modgfx.h"
+
+DLL_INTERFACE(DLL_155) {
+    /*:*/ DLL_INTERFACE_BASE(DLL_IModgfx);
+};
+
+#endif //_DLLS_155_H
