6f97a6ac125bad0c4b11266bc0aa0b1fc8d79811
Bl00D4NGEL <github@d-peters.com>
2025-11-02 23:00:23 +0100

    revert changes to dll 82

--- src/dlls/engine/82/82.c
+++ src/dlls/engine/82/82.c
@@ -9,7 +9,3 @@ void dll_82_dtor(void *dll) { }
 // offset: 0x18 | func: 0 | export: 0
 void dll_82_func_18(UNK_PTR *a0, UNK_PTR *a1, UNK_PTR *a2, UNK_PTR *a3) {
 }
-
-// offset: 0x30 | func: 1
-// hasm?
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/82/dll_82_func_30.s")
---
+++ src/dlls/engine/82/strings.s
@@ -0,0 +1,7 @@
+.text
+
+# Unused strings at the end of the .text section
+.asciz "in cave\n"
+.align 2, 0
+.asciz "out of cave\n"
+.align 2, 0
