3c638674aef470fb0550a1175d8cf529f0301c06
Banjeoin <e.odrisceoil@gmail.com>
2025-11-21 13:41:48 +0000

    Moved WL_Crystal vector maths macros to math.h

--- include/sys/math.h
+++ include/sys/math.h
@@ -39,6 +39,34 @@
  */
 #define CIRCLE_WRAP(x) WRAP(x, -0x8000, 0x8000)
 
+#define DOT_PRODUCT(vA, vB) ((vA.f[0] * vB.f[0]) + (vA.f[1] * vB.f[1]) + (vA.f[2] * vB.f[2]))
+
+#define VECTOR_ADD(vInA, vInB, vOut) vOut.f[0] = vInA.f[0] + vInB.f[0];\
+    vOut.f[1] = vInA.f[1] + vInB.f[1];\
+    vOut.f[2] = vInA.f[2] + vInB.f[2];
+
+#define VECTOR_SUBTRACT(vInA, vInB, vOut) vOut.f[0] = vInA.f[0] - vInB.f[0];\
+    vOut.f[1] = vInA.f[1] - vInB.f[1];\
+    vOut.f[2] = vInA.f[2] - vInB.f[2];
+
+#define VECTOR_MAGNITUDE_SQUARED(v) (SQ(v.f[0]) + SQ(v.f[1]) + SQ(v.f[2]))
+
+#define VECTOR_MAGNITUDE(v) sqrtf(VECTOR_MAGNITUDE_SQUARED(v))
+
+#define VECTOR_DIVIDE_BY_SCALAR(v, scalar) if (scalar != 0.0f) {\
+    v.x /= scalar;\
+    v.y /= scalar;\
+    v.z /= scalar;\
+}
+
+/*
+ * VECTOR_NORMALISE - Converts a vector into a unit vector by dividing it by its own magnitude.
+ *
+ * The vector's magnitude (prior to normalisation) will also be stored to vMagnitude
+ */
+#define VECTOR_NORMALISE(v, vMagnitude) vMagnitude = VECTOR_MAGNITUDE(v);\
+    VECTOR_DIVIDE_BY_SCALAR(v, vMagnitude)
+
 // typedef f32 MtxF[4][4];
 // For clarity:
 typedef struct {
