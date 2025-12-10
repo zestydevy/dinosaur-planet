1d27a1cf2b7f0dbe5358f207f5be46100fcd911d
Banjeoin <e.odrisceoil@gmail.com>
2025-12-10 13:06:57 +0000

    DLL 54 pickup progress

---
+++ src/dlls/engine/54_pickup/exports.s
@@ -0,0 +1,22 @@
+.option pic2
+.section ".exports"
+.global _exports
+_exports:
+
+# ctor/dtor
+.dword dll_54_ctor
+.dword dll_54_dtor
+
+# export table
+/*0*/ .dword dll_54_func_18
+/*1*/ .dword dll_54_func_64
+/*2*/ .dword dll_54_func_5DC
+/*3*/ .dword dll_54_func_664
+/*4*/ .dword dll_54_func_6A0
+/*5*/ .dword dll_54_func_6AC
+/*6*/ .dword dll_54_func_6C0
+/*7*/ .dword dll_54_func_6CC
+/*8*/ .dword dll_54_func_704
+/*9*/ .dword dll_54_func_73C
+/*10*/ .dword dll_54_func_760
+/*11*/ .dword dll_54_func_798
