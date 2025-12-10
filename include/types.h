4aba545d277bf7e5096da3ce7994349c5c5ef004
Banjeoin <e.odrisceoil@gmail.com>
2025-12-10 13:46:03 +0000

    LightAction notes

--- include/types.h
+++ include/types.h
@@ -101,36 +101,35 @@ typedef struct Triangle {
 typedef struct {
 /*0000*/ u16 unk0;
 /*0002*/ u16 unk2;
-/*0004*/ s16 unk4;
+/*0004*/ s16 unk4;  //coords? light direction/origin?
 /*0006*/ s16 unk6;
 /*0008*/ s16 unk8;
 /*000A*/ s16 unka;
-/*000C*/ s16 unkc;
-/*000E*/ u16 unke;
-/*0010*/ u16 unk10;
+/*000C*/ s16 unkc;  //y coord?
+/*000E*/ u16 unke;  //LAction.bin index
+/*0010*/ u16 unk10; //lightLayer (lights' influences can be combined when on different layers)
 /*0012*/ union {
              u16 asShort;
              u8 asByte;
          } unk12;
 /*0014*/ u8 unk14;
-/*0015*/ u8 unk15;
-/*0016*/ u8 unk16;
-/*0017*/ u8 unk17;
-/*0018*/ u8 unk18;
-/*0019*/ u8 unk19;
-/*001A*/ u8 unk1a;
+/*0015*/ u8 unk15; //colour1_r
+/*0016*/ u8 unk16; //colour1_g
+/*0017*/ u8 unk17; //colour1_b
+/*0018*/ u8 unk18; //colour2_r
+/*0019*/ u8 unk19; //colour2_g
+/*001A*/ u8 unk1a; //colour2_b
 /*001B*/ u8 unk1b;
 /*001C*/ u8 unk1c;
 /*001D*/ u8 unk1d;
 /*001E*/ u8 unk1e;
-/*001F*/ u8 unk1f;
-/*0020*/ u8 unk20;
+/*001F*/ u8 unk1f; //radius1 (falloff distance for one of the colours)
+/*0020*/ u8 unk20; //radius2 (falloff distance for the other colour)
 /*0021*/ u8 unk21;
 /*0022*/ u8 unk22;
 /*0023*/ u8 unk23;
 /*0024*/ u8 unk24[0x28-0x24];
-} NewLfxStruct;
-
+} NewLfxStruct; //LActions.bin structs used by NewLfx DLL
 
 // Size: 0x28
 typedef struct Func_80037F9C_Struct {
