ed788b9840e132a661f954b2b45144c846c2ea64
Banjeoin <e.odrisceoil@gmail.com>
2025-12-10 12:15:19 +0000

    CCkrazoabright syms

--- src/dlls/objects/457_CCkrazoabright/exports.s
+++ src/dlls/objects/457_CCkrazoabright/exports.s
@@ -4,14 +4,14 @@
 _exports:
 
 # ctor/dtor
-.dword dll_457_ctor
-.dword dll_457_dtor
+.dword CCkrazoabright_ctor
+.dword CCkrazoabright_dtor
 
 # export table
-/*0*/ .dword dll_457_setup
-/*1*/ .dword dll_457_control
-/*2*/ .dword dll_457_update
-/*3*/ .dword dll_457_print
-/*4*/ .dword dll_457_free
-/*5*/ .dword dll_457_get_model_flags
-/*6*/ .dword dll_457_get_data_size
+/*0*/ .dword CCkrazoabright_setup
+/*1*/ .dword CCkrazoabright_control
+/*2*/ .dword CCkrazoabright_update
+/*3*/ .dword CCkrazoabright_print
+/*4*/ .dword CCkrazoabright_free
+/*5*/ .dword CCkrazoabright_get_model_flags
+/*6*/ .dword CCkrazoabright_get_data_size
