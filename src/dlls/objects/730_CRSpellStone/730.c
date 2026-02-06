03de7918ed46be053f3dc653278fc4b34fce7ec0
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2026-02-06 02:57:04 -0500

    asdasd

--- src/dlls/objects/730_CRSpellStone/730.c
+++ src/dlls/objects/730_CRSpellStone/730.c
@@ -16,22 +16,22 @@ void dll_730_dtor(void *dll) { }
 
 // offset: 0x18 | func: 0 | export: 0
 void dll_730_setup(Object *self, ObjSetup *setup, s32 arg2);
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/730/CRSpellStone/dll_730_setup.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/730_CRSpellStone/dll_730_setup.s")
 
 // offset: 0x80 | func: 1 | export: 1
 void dll_730_control(Object *self);
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/730/CRSpellStone/dll_730_control.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/730_CRSpellStone/dll_730_control.s")
 
 // offset: 0x218 | func: 2 | export: 2
 void dll_730_update(Object *self) { }
 
 // offset: 0x224 | func: 3 | export: 3
 void dll_730_print(Object *self, Gfx **gdl, Mtx **mtxs, Vertex **vtxs, Triangle **pols, s8 visibility);
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/730/CRSpellStone/dll_730_print.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/730_CRSpellStone/dll_730_print.s")
 
 // offset: 0x288 | func: 4 | export: 4
 void dll_730_free(Object *self, s32 a1);
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/730/CRSpellStone/dll_730_free.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/730_CRSpellStone/dll_730_free.s")
 
 // offset: 0x2C8 | func: 5 | export: 5
 u32 dll_730_get_model_flags(Object *self) {
@@ -44,10 +44,10 @@ u32 dll_730_get_data_size(Object *self, u32 a1) {
 }
 
 // offset: 0x2EC | func: 7 | export: 7
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/730/CRSpellStone/dll_730_func_2EC.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/730_CRSpellStone/dll_730_func_2EC.s")
 
 // offset: 0x334 | func: 8 | export: 8
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/730/CRSpellStone/dll_730_func_334.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/730_CRSpellStone/dll_730_func_334.s")
 
 // offset: 0x34C | func: 9
 s32 dll_730_func_34C(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
