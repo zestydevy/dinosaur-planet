fdd4b1537c6d655476480a58fdadc213dd5255ea
Banjeoin <e.odrisceoil@gmail.com>
2026-04-21 12:41:18 +0100

    mainmenu/gameselect: enums, textable

--- src/dlls/engine/62_mainmenu/mainmenu.c
+++ src/dlls/engine/62_mainmenu/mainmenu.c
@@ -15,75 +15,128 @@
 #include "sys/rcp.h"
 #include "types.h"
 
+typedef enum {
+    //Overrides for item7 (Language)
+    MainMenu_ITEM_0_English = 0,
+    MainMenu_ITEM_1_Francais = 1,
+    MainMenu_ITEM_2_Deutsch = 2,
+    MainMenu_ITEM_3_Espanol = 3,
+    MainMenu_ITEM_5_Italiano = 4,
+
+    MainMenu_ITEM_5_Start = 5,
+    MainMenu_ITEM_6_Options = 6,
+    MainMenu_ITEM_7_Language = 7 //Gets overridden with items 0-4
+} MainMenuItemIndices;
+
+typedef enum {
+    MMTextIdx_0_English = 0,
+    MMTextIdx_1_Francais = 1,
+    MMTextIdx_2_Deutsch = 2,
+    MMTextIdx_3_Espanol = 3,
+    MMTextIdx_4_Italiano = 4,
+    MMTextIdx_5_Japanese = 5,
+    MMTextIdx_6_Press_Start = 6,
+    MMTextIdx_7_Start = 7,
+    MMTextIdx_8_Options = 8
+} MainMenuTextStrings;
+
+#define MENU_TOTAL_PICMENU_ITEMS 8
+#define MENU_TRANSITION_THRESHOLD 12
+#define MENU_TRANSITION_DURATION 35
+#define NONE -1
+
 /*0x0*/ static PicMenuItem pressStartItem[] = {
     /*0*/ NEW_PICMENU_ITEM(
-    /*textX*/ 160, /*textY*/ 165, /*innerWidth*/ 0, 
-    /*itemX*/ 160, /*itemY*/ 175, /*textureID*/ -1, /*outerWidth*/ 0, 
-    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_ALWAYS_REDRAW | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT, 
-    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT, 
-    /*upLink*/ -1, /*downLink*/ -1, /*leftLink*/ -1, /*rightLink*/ -1, /*overrideWith*/ -1
+    /*textX*/ 160, /*textY*/ 165, /*innerWidth*/ 0,
+    /*itemX*/ 160, /*itemY*/ 175, /*textureID*/ NONE, /*outerWidth*/ 0,
+    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_ALWAYS_REDRAW | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT,
+    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT,
+    /*upLink*/ NONE, /*downLink*/ NONE, /*leftLink*/ NONE, /*rightLink*/ NONE, /*overrideWith*/ NONE
     )
 };
 /*0x3C*/ static PicMenuItem mainMenuItems[] = {
+    //Language (English)
     /*0*/ NEW_PICMENU_ITEM(
-    /*textX*/ 160, /*textY*/ 180, /*innerWidth*/ 0, 
-    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ -1, /*outerWidth*/ 0, 
-    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_FLAG_INVISIBLE | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT, 
-    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT, 
-    /*upLink*/ -1, /*downLink*/ -1, /*leftLink*/ 4, /*rightLink*/ 1, /*overrideWith*/ -1
+    /*textX*/ 160, /*textY*/ 180, /*innerWidth*/ 0,
+    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ NONE, /*outerWidth*/ 0,
+    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_FLAG_INVISIBLE | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT,
+    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT,
+    /*upLink*/ NONE, /*downLink*/ NONE, /*leftLink*/ MainMenu_ITEM_5_Italiano, /*rightLink*/ MainMenu_ITEM_1_Francais, /*overrideWith*/ NONE
     ),
+
+    //Language (Français)
     /*1*/ NEW_PICMENU_ITEM(
-    /*textX*/ 160, /*textY*/ 180, /*innerWidth*/ 0, 
-    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ -1, /*outerWidth*/ 0, 
-    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_FLAG_INVISIBLE | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT, 
-    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT, 
-    /*upLink*/ -1, /*downLink*/ -1, /*leftLink*/ 0, /*rightLink*/ 2, /*overrideWith*/ -1
+    /*textX*/ 160, /*textY*/ 180, /*innerWidth*/ 0,
+    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ -1, /*outerWidth*/ 0,
+    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_FLAG_INVISIBLE | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT,
+    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT,
+    /*upLink*/ NONE, /*downLink*/ NONE, /*leftLink*/ MainMenu_ITEM_0_English, /*rightLink*/ MainMenu_ITEM_2_Deutsch, /*overrideWith*/ NONE
     ),
+
+    //Language (Deutsch)
     /*2*/ NEW_PICMENU_ITEM(
-    /*textX*/ 160, /*textY*/ 180, /*innerWidth*/ 0, 
-    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ -1, /*outerWidth*/ 0, 
-    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_FLAG_INVISIBLE | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT, 
-    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT, 
-    /*upLink*/ -1, /*downLink*/ -1, /*leftLink*/ 1, /*rightLink*/ 3, /*overrideWith*/ -1
+    /*textX*/ 160, /*textY*/ 180, /*innerWidth*/ 0,
+    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ NONE, /*outerWidth*/ 0,
+    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_FLAG_INVISIBLE | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT,
+    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT,
+    /*upLink*/ NONE, /*downLink*/ NONE, /*leftLink*/ MainMenu_ITEM_1_Francais, /*rightLink*/ MainMenu_ITEM_3_Espanol, /*overrideWith*/ NONE
     ),
+
+    //Language (Español)
     /*3*/ NEW_PICMENU_ITEM(
-    /*textX*/ 160, /*textY*/ 180, /*innerWidth*/ 0, 
-    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ -1, /*outerWidth*/ 0, 
-    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_FLAG_INVISIBLE | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT, 
-    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT, 
-    /*upLink*/ -1, /*downLink*/ -1, /*leftLink*/ 2, /*rightLink*/ 4, /*overrideWith*/ -1
+    /*textX*/ 160, /*textY*/ 180, /*innerWidth*/ 0,
+    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ NONE, /*outerWidth*/ 0,
+    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_FLAG_INVISIBLE | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT,
+    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT,
+    /*upLink*/ NONE, /*downLink*/ NONE, /*leftLink*/ MainMenu_ITEM_2_Deutsch, /*rightLink*/ MainMenu_ITEM_5_Italiano, /*overrideWith*/ NONE
     ),
+
+    //Language (Italiano)
     /*4*/ NEW_PICMENU_ITEM(
-    /*textX*/ 160, /*textY*/ 180, /*innerWidth*/ 0, 
-    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ -1, /*outerWidth*/ 0, 
-    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_FLAG_INVISIBLE | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT, 
-    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT, 
-    /*upLink*/ -1, /*downLink*/ -1, /*leftLink*/ 3, /*rightLink*/ 0, /*overrideWith*/ -1
+    /*textX*/ 160, /*textY*/ 180, /*innerWidth*/ 0,
+    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ NONE, /*outerWidth*/ 0,
+    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_FLAG_INVISIBLE | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT,
+    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT,
+    /*upLink*/ NONE, /*downLink*/ NONE, /*leftLink*/ MainMenu_ITEM_3_Espanol, /*rightLink*/ MainMenu_ITEM_0_English, /*overrideWith*/ NONE
     ),
+
+    //Start
     /*5*/ NEW_PICMENU_ITEM(
-    /*textX*/ 160, /*textY*/ 150, /*innerWidth*/ 0, 
-    /*itemX*/ 160, /*itemY*/ 180, /*textureID*/ -1, /*outerWidth*/ 0, 
-    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_ALWAYS_REDRAW | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT, 
-    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT, 
-    /*upLink*/ -1, /*downLink*/ 6, /*leftLink*/ -1, /*rightLink*/ -1, /*overrideWith*/ -1
+    /*textX*/ 160, /*textY*/ 150, /*innerWidth*/ 0,
+    /*itemX*/ 160, /*itemY*/ 180, /*textureID*/ NONE, /*outerWidth*/ 0,
+    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_ALWAYS_REDRAW | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT,
+    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT,
+    /*upLink*/ NONE, /*downLink*/ MainMenu_ITEM_6_Options, /*leftLink*/ NONE, /*rightLink*/ NONE, /*overrideWith*/ NONE
     ),
+
+    //Options
     /*6*/ NEW_PICMENU_ITEM(
-    /*textX*/ 160, /*textY*/ 165, /*innerWidth*/ 0, 
-    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ -1, /*outerWidth*/ 0, 
-    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_ALWAYS_REDRAW | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT, 
-    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT, 
-    /*upLink*/ 5, /*downLink*/ 7, /*leftLink*/ -1, /*rightLink*/ -1, /*overrideWith*/ -1
+    /*textX*/ 160, /*textY*/ 165, /*innerWidth*/ 0,
+    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ NONE, /*outerWidth*/ 0,
+    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_ALWAYS_REDRAW | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT,
+    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT,
+    /*upLink*/ MainMenu_ITEM_5_Start, /*downLink*/ MainMenu_ITEM_7_Language, /*leftLink*/ NONE, /*rightLink*/ NONE, /*overrideWith*/ NONE
     ),
+
+    //Language
     /*7*/ NEW_PICMENU_ITEM(
-    /*textX*/ 160, /*textY*/ 180, /*innerWidth*/ 0, 
-    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ -1, /*outerWidth*/ 0, 
-    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_ALWAYS_REDRAW | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT, 
-    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT, 
-    /*upLink*/ 6, /*downLink*/ -1, /*leftLink*/ -1, /*rightLink*/ -1, /*overrideWith*/ 0
+    /*textX*/ 160, /*textY*/ 180, /*innerWidth*/ 0,
+    /*itemX*/ 160, /*itemY*/ 187, /*textureID*/ NONE, /*outerWidth*/ 0,
+    /*flags*/ PICMENU_ALIGN_TEXT_CENTER | PICMENU_ALWAYS_REDRAW | PICMENU_USE_TEXT_COLOR | PICMENU_RAISED_EFFECT,
+    /*fontID*/ FONT_FUN_FONT, /*highlightFontID*/ FONT_FUN_FONT,
+    /*upLink*/ MainMenu_ITEM_6_Options, /*downLink*/ NONE, /*leftLink*/ NONE, /*rightLink*/ NONE, /*overrideWith*/ MainMenu_ITEM_0_English
     ),
 };
+
 /*0x21C*/ static s16 gametextLineIndices[] = {
-    0x0000, 0x0001, 0x0002, 0x0003, 0x0004, 0x0007, 0x0008
+    MMTextIdx_0_English,
+    MMTextIdx_1_Francais,
+    MMTextIdx_2_Deutsch,
+    MMTextIdx_3_Espanol,
+    MMTextIdx_4_Italiano,
+    MMTextIdx_7_Start,
+    MMTextIdx_8_Options,
+    MMTextIdx_0_English
 };
 
 /*0x0*/ static u8 showDPLogo;
@@ -93,9 +146,6 @@
 /*0xD*/ static s8 nextMenuID;
 /*0x10*/ static Texture* logoDinosaurPlanet;
 
-#define MENU_TRANSITION_THRESHOLD 12
-#define MENU_TRANSITION_DURATION 35
-
 static void mainmenu_clean_up(void);
 
 // offset: 0x0 | ctor
@@ -104,7 +154,7 @@ void mainmenu_ctor(void *dll) {
     s32 index;
 
     total_strings = 8;
-    
+
     logoDinosaurPlanet = tex_load_deferred(TEXTABLE_C5_DinosaurPlanetLogo);
     rcp_set_border_color(0, 0, 0);
 
@@ -114,22 +164,22 @@ void mainmenu_ctor(void *dll) {
     gametext = gDLL_21_Gametext->vtbl->get_chunk(GAMETEXT_0EE_Menu_Title_Screen);
 
     //Set "Press Start" text
-    pressStartItem->text = gametext->strings[6];
+    pressStartItem->text = gametext->strings[MMTextIdx_6_Press_Start];
 
     for (index = 0; index < total_strings; index++){
         mainMenuItems[index].text = gametext->strings[gametextLineIndices[index]];
     }
-    
-    //Hide the DP logo at first when entering from the Rareware screen 
+
+    //Hide the DP logo at first when entering from the Rareware screen
     if (menu_get_previous() == MENU_RAREWARE) {
         gDLL_74_Picmenu->vtbl->set_items(pressStartItem, 1, 0, 0, 0, 0, 0xB7, 0x8B, 0x61, 0xFF, 0xD7, 0x3D);
         showDPLogo = FALSE;
     } else {
-        mainMenuItems[7].overrideWith = options->languageID; //Set language index
+        mainMenuItems[MainMenu_ITEM_7_Language].overrideWith = options->languageID; //Set language index
         gDLL_74_Picmenu->vtbl->set_items(mainMenuItems, 8, 5, 0, 0, 0, 0xB7, 0x8B, 0x61, 0xFF, 0xD7, 0x3D);
         showDPLogo = TRUE;
     }
-    
+
     nextMenuID = NULL;
     sExitTransitionTimer = 0;
 }
@@ -139,7 +189,7 @@ void mainmenu_dtor(void *dll){ }
 
 // offset: 0x31C | func: 0 | export: 0
 s32 mainmenu_update1(void) {
-    s32 items;
+    s32 temp;
     s32 index;
     s32 action;
     s32 lineIndex;
@@ -184,44 +234,44 @@ s32 mainmenu_update1(void) {
         if (showDPLogo == FALSE){
             if (action == PICMENU_ACTION_SELECT) {
                 gDLL_74_Picmenu->vtbl->clear_items();
-                mainMenuItems[7].overrideWith = options->languageID;
+                mainMenuItems[MainMenu_ITEM_7_Language].overrideWith = options->languageID;
                 gDLL_74_Picmenu->vtbl->set_items(mainMenuItems, 8, 5, NULL, 0, 0, 0xB7, 0x8B, 0x61, 0xFF, 0xD7, 0x3D);
                 showDPLogo = TRUE;
             }
         } else {
             if (action == PICMENU_ACTION_SELECT) {
-    
-                items = 1;
+
+                temp = 1;
                 switch (gDLL_74_Picmenu->vtbl->get_selected_item()) {
-                    case 5:
-                        gDLL_28_ScreenFade->vtbl->fade(20, items);
+                    case MainMenu_ITEM_5_Start:
+                        gDLL_28_ScreenFade->vtbl->fade(20, temp);
                         nextMenuID = MENU_GAME_SELECT;
                         sExitTransitionTimer = MENU_TRANSITION_DURATION;
                         return 0;
-                    case 6:
-                        gDLL_28_ScreenFade->vtbl->fade(20, items);
+                    case MainMenu_ITEM_6_Options:
+                        gDLL_28_ScreenFade->vtbl->fade(20, temp);
                         nextMenuID = MENU_OPTIONS;
                         sExitTransitionTimer = MENU_TRANSITION_DURATION;
                         return 0;
                 }
             }
-    
+
             //Changing language
-            options->languageID = gDLL_74_Picmenu->vtbl->get_item_override(7);
+            options->languageID = gDLL_74_Picmenu->vtbl->get_item_override(MainMenu_ITEM_7_Language);
             if (gDLL_21_Gametext->vtbl->curr_bank() != options->languageID) {
                 gDLL_21_Gametext->vtbl->set_bank(options->languageID);
                 mmFree(gametext);
                 gametext = gDLL_21_Gametext->vtbl->get_chunk(GAMETEXT_0EE_Menu_Title_Screen);
-    
-                items = 8;
-                for (index = 0; index < items; index++){
+
+                temp = MENU_TOTAL_PICMENU_ITEMS;
+                for (index = 0; index < temp; index++){
                     mainMenuItems[index].text = gametext->strings[gametextLineIndices[index]];
                 }
-                
+
                 gDLL_74_Picmenu->vtbl->update_text(mainMenuItems);
             }
         }
-        
+
         return 0;
     }
 }
