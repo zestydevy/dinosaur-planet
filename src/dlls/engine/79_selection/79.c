06640572ca9a7e8fc1892ed15544bbfdd6ae08f6
Banjeoin <e.odrisceoil@gmail.com>
2026-04-27 19:54:59 +0100

    DLL 79 coord macros

--- src/dlls/engine/79_selection/79.c
+++ src/dlls/engine/79_selection/79.c
@@ -6,6 +6,73 @@
 #include "sys/gfx/textable.h"
 #include "sys/menu.h"
 
+/** COORD MACROS */
+
+#define SHADOW_X -1
+#define SHADOW_Y -1
+
+// Confirm/Cancel (left-aligned)
+
+#define CONFIRM_LEFT_A_X 30
+#define CONFIRM_LEFT_A_Y 168
+
+#define CONFIRM_LEFT_B_X 30
+#define CONFIRM_LEFT_B_Y 142
+
+#define CONFIRM_LEFT_TEXT_CANCEL_X 56
+#define CONFIRM_LEFT_TEXT_CANCEL_Y 148
+
+#define CONFIRM_LEFT_TEXT_CONFIRM_X 56
+#define CONFIRM_LEFT_TEXT_CONFIRM_Y 174
+
+// Confirm/Cancel (right-aligned)
+
+#define CONFIRM_RIGHT_A_X 218
+#define CONFIRM_RIGHT_A_Y 168
+
+#define CONFIRM_RIGHT_B_X 218
+#define CONFIRM_RIGHT_B_Y 142
+
+#define CONFIRM_RIGHT_TEXT_CANCEL_X 244
+#define CONFIRM_RIGHT_TEXT_CANCEL_Y 148
+
+#define CONFIRM_RIGHT_TEXT_CONFIRM_X 244
+#define CONFIRM_RIGHT_TEXT_CONFIRM_Y 174
+
+// SwapStone Choices
+
+#define SWAPSTONE_STICK_X 157
+#define SWAPSTONE_STICK_Y 78
+
+#define SWAPSTONE_TEXT_SWAP_X 149
+#define SWAPSTONE_TEXT_SWAP_Y 84
+
+#define SWAPSTONE_TEXT_SHOP_X 153
+#define SWAPSTONE_TEXT_SHOP_Y 66
+
+#define SWAPSTONE_TEXT_WARP_X 187
+#define SWAPSTONE_TEXT_WARP_Y 84
+
+// Confirm/Cancel (right-aligned)
+
+#define HAGGLE_A_X 218
+#define HAGGLE_A_Y 94
+
+#define HAGGLE_B_X 218
+#define HAGGLE_B_Y 68
+
+#define HAGGLE_STICK_X 218
+#define HAGGLE_STICK_Y 42
+
+#define HAGGLE_TEXT_CHANGE_X 244
+#define HAGGLE_TEXT_CHANGE_Y 48
+
+#define HAGGLE_TEXT_CANCEL_X 244
+#define HAGGLE_TEXT_CANCEL_Y 74
+
+#define HAGGLE_TEXT_CONFIRM_X 244
+#define HAGGLE_TEXT_CONFIRM_Y 100
+
 /*0x0*/ static u8 sMenuState;
 /*0x4*/ static Texture* sTexButtonA;
 /*0x8*/ static Texture* sTexButtonB;
@@ -92,70 +159,70 @@ void selection_draw(Gfx** gdl, Mtx** mtx, Vertex** vtx) {
     switch (sMenuState) {
     case SelectionMenu_STATE_3_Confirm_Left:
         //Draw B button & A button
-        rcp_screen_full_write(gdl, sTexButtonB, 30, 142, 0, dFrameButtonB >> 8, (u8)sOpacity, 0);
-        rcp_screen_full_write(gdl, sTexButtonA, 30, 168, 0, dFrameButtonA >> 8, (u8)sOpacity, 0);
+        rcp_screen_full_write(gdl, sTexButtonB, CONFIRM_LEFT_B_X, CONFIRM_LEFT_B_Y, 0, dFrameButtonB >> 8, (u8)sOpacity, 0);
+        rcp_screen_full_write(gdl, sTexButtonA, CONFIRM_LEFT_A_X, CONFIRM_LEFT_A_Y, 0, dFrameButtonA >> 8, (u8)sOpacity, 0);
         
         //"Confirm" / "Cancel" text (drop-shadow)
         font_window_set_text_colour(1, 0xFF, 0xD7, 0x3D, 0xFF, (u8)sOpacity);
-        font_window_add_string_xy(1, 56, 148, sGametext->strings[SelectionMenu_STRING_CANCEL],  1, 0);
-        font_window_add_string_xy(1, 56, 174, sGametext->strings[SelectionMenu_STRING_CONFIRM], 1, 0);
+        font_window_add_string_xy(1, CONFIRM_LEFT_TEXT_CANCEL_X,  CONFIRM_LEFT_TEXT_CANCEL_Y,  sGametext->strings[SelectionMenu_STRING_CANCEL],  1, ALIGN_TOP_LEFT);
+        font_window_add_string_xy(1, CONFIRM_LEFT_TEXT_CONFIRM_X, CONFIRM_LEFT_TEXT_CONFIRM_Y, sGametext->strings[SelectionMenu_STRING_CONFIRM], 1, ALIGN_TOP_LEFT);
         
         //"Confirm" / "Cancel" text (main)
         font_window_set_text_colour(1, 0, 0, 0, 0xFF, (((u8)sOpacity) * 0x96) >> 8);
-        font_window_add_string_xy(1, 55, 147, sGametext->strings[SelectionMenu_STRING_CANCEL],  2, 0);
-        font_window_add_string_xy(1, 55, 173, sGametext->strings[SelectionMenu_STRING_CONFIRM], 2, 0);
+        font_window_add_string_xy(1, (CONFIRM_LEFT_TEXT_CANCEL_X  + SHADOW_X), (CONFIRM_LEFT_TEXT_CANCEL_Y  + SHADOW_Y), sGametext->strings[SelectionMenu_STRING_CANCEL],  2, ALIGN_TOP_LEFT);
+        font_window_add_string_xy(1, (CONFIRM_LEFT_TEXT_CONFIRM_X + SHADOW_X), (CONFIRM_LEFT_TEXT_CONFIRM_Y + SHADOW_Y), sGametext->strings[SelectionMenu_STRING_CONFIRM], 2, ALIGN_TOP_LEFT);
         break;
 
     case SelectionMenu_STATE_2_Confirm_Right:
         //Draw B button & A button
-        rcp_screen_full_write(gdl, sTexButtonB, 218, 142, 0, dFrameButtonB >> 8, (u8)sOpacity, 0);
-        rcp_screen_full_write(gdl, sTexButtonA, 218, 168, 0, dFrameButtonA >> 8, (u8)sOpacity, 0);
+        rcp_screen_full_write(gdl, sTexButtonB, CONFIRM_RIGHT_B_X, CONFIRM_RIGHT_B_Y, 0, dFrameButtonB >> 8, (u8)sOpacity, 0);
+        rcp_screen_full_write(gdl, sTexButtonA, CONFIRM_RIGHT_A_X, CONFIRM_RIGHT_A_Y, 0, dFrameButtonA >> 8, (u8)sOpacity, 0);
         
         //"Confirm" / "Cancel" text (drop-shadow)
         font_window_set_text_colour(1, 0xFF, 0xD7, 0x3D, 0xFF, (u8)sOpacity);
-        font_window_add_string_xy(1, 244, 148, sGametext->strings[SelectionMenu_STRING_CANCEL],  1, 0);
-        font_window_add_string_xy(1, 244, 174, sGametext->strings[SelectionMenu_STRING_CONFIRM], 1, 0);
+        font_window_add_string_xy(1, CONFIRM_RIGHT_TEXT_CANCEL_X,  CONFIRM_RIGHT_TEXT_CANCEL_Y,  sGametext->strings[SelectionMenu_STRING_CANCEL],  1, ALIGN_TOP_LEFT);
+        font_window_add_string_xy(1, CONFIRM_RIGHT_TEXT_CONFIRM_X, CONFIRM_RIGHT_TEXT_CONFIRM_Y, sGametext->strings[SelectionMenu_STRING_CONFIRM], 1, ALIGN_TOP_LEFT);
         
         //"Confirm" / "Cancel" text (main)
         font_window_set_text_colour(1, 0, 0, 0, 0xFF, (((u8)sOpacity) * 0x96) >> 8);
-        font_window_add_string_xy(1, 243, 147, sGametext->strings[SelectionMenu_STRING_CANCEL],  2, 0);
-        font_window_add_string_xy(1, 243, 173, sGametext->strings[SelectionMenu_STRING_CONFIRM], 2, 0);
+        font_window_add_string_xy(1, (CONFIRM_RIGHT_TEXT_CANCEL_X  + SHADOW_X), (CONFIRM_RIGHT_TEXT_CANCEL_Y  + SHADOW_Y), sGametext->strings[SelectionMenu_STRING_CANCEL],  2, ALIGN_TOP_LEFT);
+        font_window_add_string_xy(1, (CONFIRM_RIGHT_TEXT_CONFIRM_X + SHADOW_X), (CONFIRM_RIGHT_TEXT_CONFIRM_Y + SHADOW_Y), sGametext->strings[SelectionMenu_STRING_CONFIRM], 2, ALIGN_TOP_LEFT);
         break;
 
     case SelectionMenu_STATE_1_SwapStone_Choices:
         //Draw control stick
-        rcp_screen_full_write(gdl, sTexControlStick, 0x9D, 0x4E, 0, dFrameControlStick >> 8, (u8)sOpacity, 0);
+        rcp_screen_full_write(gdl, sTexControlStick, SWAPSTONE_STICK_X, SWAPSTONE_STICK_Y, 0, dFrameControlStick >> 8, (u8)sOpacity, 0);
         
         //"Warp" / "Swap" / "Shop" text (drop-shadow)
         font_window_set_text_colour(1, 0xFF, 0xD7, 0x3D, 0xFF, (u8)sOpacity);
-        font_window_add_string_xy(1, 187, 84,  sGametext->strings[SelectionMenu_STRING_WARP], 1, 0);
-        font_window_add_string_xy(1, 149, 84,  sGametext->strings[SelectionMenu_STRING_SWAP], 1, 1);
-        font_window_add_string_xy(1, 153, 66,  sGametext->strings[SelectionMenu_STRING_SHOP], 1, -0x8000);
+        font_window_add_string_xy(1, SWAPSTONE_TEXT_WARP_X, SWAPSTONE_TEXT_WARP_Y,  sGametext->strings[SelectionMenu_STRING_WARP], 1, ALIGN_TOP_LEFT);
+        font_window_add_string_xy(1, SWAPSTONE_TEXT_SWAP_X, SWAPSTONE_TEXT_SWAP_Y,  sGametext->strings[SelectionMenu_STRING_SWAP], 1, ALIGN_TOP_RIGHT);
+        font_window_add_string_xy(1, SWAPSTONE_TEXT_SHOP_X, SWAPSTONE_TEXT_SHOP_Y,  sGametext->strings[SelectionMenu_STRING_SHOP], 1, -0x8000);
         
         //"Warp" / "Swap" / "Shop" text (main)
         font_window_set_text_colour(1, 0, 0, 0, 0xFF, (((u8)sOpacity) * 0x96) >> 8);
-        font_window_add_string_xy(1, 186, 83,  sGametext->strings[SelectionMenu_STRING_WARP], 2, 0);
-        font_window_add_string_xy(1, 148, 83,  sGametext->strings[SelectionMenu_STRING_SWAP], 2, 1);
-        font_window_add_string_xy(1, 152, 65,  sGametext->strings[SelectionMenu_STRING_SHOP], 2, -0x8000);
+        font_window_add_string_xy(1, (SWAPSTONE_TEXT_WARP_X + SHADOW_X), (SWAPSTONE_TEXT_WARP_Y + SHADOW_Y), sGametext->strings[SelectionMenu_STRING_WARP], 2, ALIGN_TOP_LEFT);
+        font_window_add_string_xy(1, (SWAPSTONE_TEXT_SWAP_X + SHADOW_X), (SWAPSTONE_TEXT_SWAP_Y + SHADOW_Y), sGametext->strings[SelectionMenu_STRING_SWAP], 2, ALIGN_TOP_RIGHT);
+        font_window_add_string_xy(1, (SWAPSTONE_TEXT_SHOP_X + SHADOW_X), (SWAPSTONE_TEXT_SHOP_Y + SHADOW_Y), sGametext->strings[SelectionMenu_STRING_SHOP], 2, -0x8000);
         break;
 
     case SelectionMenu_STATE_4_Confirm_ShopKeeper:
         //Draw control stick, B button, & A button
-        rcp_screen_full_write(gdl, sTexControlStick, 218, 42, 0, dFrameControlStick >> 8, (u8)sOpacity, 0);
-        rcp_screen_full_write(gdl, sTexButtonB,      218, 68, 0, dFrameButtonB >> 8, (u8)sOpacity, 0);
-        rcp_screen_full_write(gdl, sTexButtonA,      218, 94, 0, dFrameButtonA >> 8, (u8)sOpacity, 0);
+        rcp_screen_full_write(gdl, sTexControlStick, HAGGLE_STICK_X, HAGGLE_STICK_Y, 0, dFrameControlStick >> 8, (u8)sOpacity, 0);
+        rcp_screen_full_write(gdl, sTexButtonB,      HAGGLE_B_X,     HAGGLE_B_Y,     0, dFrameButtonB >> 8, (u8)sOpacity, 0);
+        rcp_screen_full_write(gdl, sTexButtonA,      HAGGLE_A_X,     HAGGLE_A_Y,     0, dFrameButtonA >> 8, (u8)sOpacity, 0);
         
         //"Change" / "Cancel" / "Confirm" text (drop-shadow)
         font_window_set_text_colour(1, 0xFF, 0xD7, 0x3D, 0xFF, (u8)sOpacity);
-        font_window_add_string_xy(1, 244, 48,  sGametext->strings[SelectionMenu_STRING_CHANGE],  1, 0);
-        font_window_add_string_xy(1, 244, 74,  sGametext->strings[SelectionMenu_STRING_CANCEL],  1, 0);
-        font_window_add_string_xy(1, 244, 100, sGametext->strings[SelectionMenu_STRING_CONFIRM], 1, 0);
+        font_window_add_string_xy(1, HAGGLE_TEXT_CHANGE_X,  HAGGLE_TEXT_CHANGE_Y,  sGametext->strings[SelectionMenu_STRING_CHANGE],  1, ALIGN_TOP_LEFT);
+        font_window_add_string_xy(1, HAGGLE_TEXT_CANCEL_X,  HAGGLE_TEXT_CANCEL_Y,  sGametext->strings[SelectionMenu_STRING_CANCEL],  1, ALIGN_TOP_LEFT);
+        font_window_add_string_xy(1, HAGGLE_TEXT_CONFIRM_X, HAGGLE_TEXT_CONFIRM_Y, sGametext->strings[SelectionMenu_STRING_CONFIRM], 1, ALIGN_TOP_LEFT);
         
         //"Change" / "Cancel" / "Confirm" text (main)
         font_window_set_text_colour(1, 0, 0, 0, 0xFF, (((u8)sOpacity) * 0x96) >> 8);
-        font_window_add_string_xy(1, 243, 47,  sGametext->strings[SelectionMenu_STRING_CHANGE],  1, 0);
-        font_window_add_string_xy(1, 243, 73,  sGametext->strings[SelectionMenu_STRING_CANCEL],  2, 0);
-        font_window_add_string_xy(1, 243, 99,  sGametext->strings[SelectionMenu_STRING_CONFIRM], 2, 0);
+        font_window_add_string_xy(1, (HAGGLE_TEXT_CHANGE_X  + SHADOW_X), (HAGGLE_TEXT_CHANGE_Y  + SHADOW_Y), sGametext->strings[SelectionMenu_STRING_CHANGE],  1, ALIGN_TOP_LEFT);
+        font_window_add_string_xy(1, (HAGGLE_TEXT_CANCEL_X  + SHADOW_X), (HAGGLE_TEXT_CANCEL_Y  + SHADOW_Y), sGametext->strings[SelectionMenu_STRING_CANCEL],  2, ALIGN_TOP_LEFT);
+        font_window_add_string_xy(1, (HAGGLE_TEXT_CONFIRM_X + SHADOW_X), (HAGGLE_TEXT_CONFIRM_Y + SHADOW_Y), sGametext->strings[SelectionMenu_STRING_CONFIRM], 2, ALIGN_TOP_LEFT);
         break;
     }
     
