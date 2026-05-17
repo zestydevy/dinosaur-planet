f9a680e471356abfe7e409084f82ce9e174e025e
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2026-05-16 22:08:58 -0400

    dll_53_func_B84, dll_53_func_B90, dll_53_func_BA4, dll_53_func_CDC, dll_53_func_E9C, dll_53_func_106C, dll_53_func_1130, dll_53_func_1CC8, dll_53_func_1DEC

--- src/dlls/engine/53_movelib/movelib.c
+++ src/dlls/engine/53_movelib/movelib.c
@@ -7,7 +7,7 @@
 // TODO: move to header
 typedef struct {
     f32 unk0;
-    u8 _unk4[0x10 - 0x4];
+    Vec3f unk4;
     Vec3f unk10;
     HeadAnimation unk1C;
     u8 _unk40[0x454 - 0x40];
@@ -30,9 +30,15 @@ typedef struct {
     s32 unk4B4;
 } MoveLibData;
 
-/*0x0*/ static u32 data_0[] = {
-    0x00230023, 0x00230023, 0x00230023, 0x00230023, 0x00230023, 0x00000000, 0x00000000, 0x00000000
+/*0x0*/ static s16 data_0[] = {
+    0x0023, 0x0023, 0x0023, 0x0023, 
+    0x0023, 0x0023, 0x0023, 0x0023, 
+    0x0023, 0x0023, 0x0000, 0x0000, 
+    0x0000, 0x0000, 0x0000, 0x0000
 };
+
+static void dll_53_func_106C(Object* actor, Object* animObj, AnimObj_Data* animObjData);
+void dll_53_func_1E70(MoveLibData* arg0, s16* arg1, s16* arg2, s32 arg3);
 
 // offset: 0x0 | ctor
 void dll_53_ctor(void *dll) {
@@ -108,7 +114,7 @@ static s32 dll_53_func_18(Object* arg0, Object* arg1, s32* arg2, MoveLibData* ar
 }
 
 // offset: 0x3B4 | func: 1
-void dll_53_func_3B4(Object* arg0, Object* arg1, MoveLibData* arg2, s16* arg3) {
+static void dll_53_func_3B4(Object* arg0, Object* arg1, MoveLibData* arg2, s16* arg3) {
     s32* temp_a1 = func_800349B0();
     switch (arg2->unk498) {
     case 3:
@@ -264,25 +270,170 @@ void dll_53_func_4B8(Object* arg0, MoveLibData* arg1) {
 }
 
 // offset: 0xB84 | func: 3 | export: 1
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_B84.s")
+void dll_53_func_B84(MoveLibData* arg0, Object* arg1) {
+    arg0->unk4A0 = arg1;
+}
 
 // offset: 0xB90 | func: 4 | export: 5
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_B90.s")
+void dll_53_func_B90(MoveLibData* arg0, s32 arg1, s32 arg2) {
+    arg0->unk4AC = arg1;
+    arg0->unk4B0 = arg2;
+    arg0->unk4B4 = arg1;
+}
 
 // offset: 0xBA4 | func: 5 | export: 2
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_BA4.s")
+void dll_53_func_BA4(Object* arg0, MoveLibData* arg1, s16 arg2, s16 arg3, s32 arg4) {
+    arg1->unk4A4 = arg2;
+    arg1->unk4A6 = arg3;
+    arg1->unk4A8 = arg4;
+    arg1->unk494 = 0;
+    arg1->unk490 = 0;
+    arg1->unk49C = NULL;
+    arg1->unk4A0 = NULL;
+    arg1->unk498 = 0;
+    arg1->unk499 = 1;
+    arg1->unk4.z = 0.0f;
+    arg1->unk4.y = 0.0f;
+    arg1->unk4.x = 0.0f;
+    arg1->unk0 = 0/*.0f*/;
+    arg1->unk4AC = -1;
+    func_800332A4(arg0, func_800349B0(), arg4);
+    func_80033224(arg0, func_800349B0(), arg4, &arg1->unk1C);
+    func_80033350(&arg1->unk1C, arg1->unk4A8, 0, 0);
+    dll_53_func_1E70(arg1, data_0, data_0, 8);
+}
 
 // offset: 0xCDC | func: 6 | export: 3
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_CDC.s")
+void dll_53_func_CDC(Object* arg0, MoveLibData* arg1, s32 arg2) {
+    Vec3f sp4C;
+    Vec3f sp3C;
+    s16 sp38[3];
+
+    if (arg1->unk499 != 0) {
+        func_800332A4(arg0, func_800349B0(), arg1->unk4A8);
+        func_80031F6C(arg0, arg2, &sp3C.x, &sp3C.y, &sp3C.z, 0);
+        func_80031F6C(arg0, arg2 + 1, &sp4C.f[0], &sp4C.f[1], &sp4C.f[2], 0);
+        arg1->unk4.x = ((sp4C.f[0] + (3.0f * sp3C.x)) * 0.25f);
+        arg1->unk4.y = sp3C.y;
+        arg1->unk4.z = ((sp4C.f[2] + (3.0f * sp3C.z)) * 0.25f);
+        arg1->unk4.x = (arg1->unk4.x - arg0->srt.transl.x);
+        arg1->unk4.y = (arg1->unk4.y - arg0->srt.transl.y);
+        arg1->unk4.z = (arg1->unk4.z - arg0->srt.transl.z);
+        sp38[0] = -arg0->srt.roll;
+        sp38[1] = -arg0->srt.pitch;
+        sp38[2] = -arg0->srt.yaw;
+        rotate_vec3((const SRT*)sp38, arg1->unk4.f);
+        arg1->unk499 = 0;
+    }
+    func_80031F6C(arg0, arg2, &sp3C.x, &sp3C.y, &sp3C.z, 0);
+    arg1->unk10.x = sp3C.x;
+    arg1->unk10.y = sp3C.y;
+    arg1->unk10.z = sp3C.z;
+}
 
 // offset: 0xE9C | func: 7 | export: 4
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_E9C.s")
+s32 dll_53_func_E9C(Object* arg0, AnimObj_Data* arg1, MoveLibData* arg2, s16 arg3, s16 arg4) {
+    Object* temp_v0;
+    s16 sp38[2];
+    s16* temp_v0_3;
+
+    temp_v0 = get_player();
+    sp38[0] = arg3;
+    sp38[1] = arg4;
+    arg0->objhitInfo->unk58 |= 1;
+    if (arg1->unk62 == 4) {
+        arg2->unk490 = 0x50;
+        arg1->unk7A &= ~0x8;
+        arg1->unk7A &= ~0x2;
+        arg2->unk498 = 3;
+        arg1->unk62 = 5;
+        arg1->unk7A &= ~0x4;
+        arg1->unkF4 = dll_53_func_106C;
+        return 1;
+    }
+    if (arg1->unk62 == 5) {
+        if ((arg2->unk498 >= 2) && (arg2->unk498 < 8)) {
+            dll_53_func_3B4(arg0, temp_v0, arg2, sp38);
+            func_80024108(arg0, arg2->unk0, (f32) gUpdateRate, NULL);
+            if (arg2->unk498 == 7) {
+                arg1->unk7A |= 8;
+                temp_v0_3 = func_80034804(arg0, 0);
+                arg1->unk120 = temp_v0_3[1];
+                arg1->unk122 = temp_v0_3[0];
+                arg2->unk498 = 0;
+                arg1->unk62 = 0;
+                arg1->unk7A |= 4;
+                return 0;
+            }
+            return 1;
+        }
+    }
+    return 0;
+}
 
 // offset: 0x106C | func: 8
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_106C.s")
+static void dll_53_func_106C(Object* actor, Object* animObj, AnimObj_Data* animObjData) {
+    MoveLibData* sp2C;
+    s32* sp28;
+
+    sp28 = func_800349B0();
+    sp2C = actor->data;
+    gDLL_2_Camera->vtbl->set_target_object(NULL);
+    sp2C->unk498 = 0;
+    func_80033224(actor, sp28, sp2C->unk4A8, &sp2C->unk1C);
+    sp2C->unk490 = 0x50;
+    func_80033350(&sp2C->unk1C, sp2C->unk4A8, 0, 0);
+}
 
 // offset: 0x1130 | func: 9 | export: 10
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_1130.s")
+s32 dll_53_func_1130(Object* arg0, SRT* arg1, f32 arg2, s32 arg3, f32* arg4, u8* arg5) {
+    Vec3f sp3C;
+    f32 sp38;
+    f32 sp34;
+    s16 var_a0;
+
+    if (arg1 == NULL) {
+        return 0;
+    }
+    sp3C.f[0] = arg1->transl.x - arg0->srt.transl.x;
+    sp3C.f[1] = arg1->transl.f[1] - arg0->srt.transl.f[1];
+    sp3C.f[2] = arg1->transl.f[2] - arg0->srt.transl.f[2];
+    sp38 = sqrtf(SQ(sp3C.f[0]) + SQ(sp3C.f[1]) + SQ(sp3C.f[2]));
+    if (sp38 < (arg2 * 5.0f)) {
+        arg0->srt.transl.x = arg1->transl.x;
+        arg0->srt.transl.f[1] = arg1->transl.f[1];
+        arg0->srt.transl.f[2] = arg1->transl.f[2];
+        if ((*arg5 & 1) && (func_80058680(arg0, arg0->srt.transl.x, arg0->srt.transl.f[1], arg0->srt.transl.f[2], &sp34, 0U) == 0)) {
+            arg0->srt.transl.f[1] -= sp34;
+        }
+        return 1;
+    }
+    guNormalize(&sp3C.f[0], &sp3C.f[1], &sp3C.f[2]);
+    arg0->velocity.x = sp3C.f[0] * (arg2 * gUpdateRateF);
+    arg0->velocity.f[1] = sp3C.f[1] * (arg2 * gUpdateRateF);
+    arg0->velocity.f[2] = sp3C.f[2] * (arg2 * gUpdateRateF);
+    if ((*arg5 & 1) && (func_80058680(arg0, arg0->srt.transl.x, arg0->srt.transl.f[1], arg0->srt.transl.f[2], &sp34, 0U) == 0)) {
+        arg0->srt.transl.f[1] -= sp34;
+    }
+    if (*arg5 & 2) {
+        var_a0 = arg1->yaw - (u16)arg0->srt.yaw;
+        CIRCLE_WRAP(var_a0);
+        arg0->srt.yaw = (s16) (s32) ((f32) arg0->srt.yaw + ((((f32) var_a0 + 0.5f) * (arg2 * gUpdateRateF)) / sp38));
+    }
+    obj_move(arg0, arg0->velocity.x, arg0->velocity.f[1], arg0->velocity.f[2]);
+    if (arg3 != -1) {
+        if (arg3 != arg0->curModAnimId) {
+            func_80023D30(arg0, arg3, 0.0f, 0U);
+        }
+        var_a0 = (u16)arctan2_f(sp3C.f[0], sp3C.f[2]);
+        var_a0 = arg0->srt.yaw - (var_a0 & 0xFFFF);
+        CIRCLE_WRAP(var_a0);
+        arg2 *= -fcos16_precise(var_a0);
+        func_8002493C(arg0, arg2, arg4);
+    }
+    
+    return 0;
+}
 
 // offset: 0x14F4 | func: 10 | export: 11
 #pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_14F4.s")
@@ -303,16 +454,109 @@ void dll_53_func_4B8(Object* arg0, MoveLibData* arg1) {
 #pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_1C0C.s")
 
 // offset: 0x1CC8 | func: 16 | export: 9
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_1CC8.s")
+s32 dll_53_func_1CC8(s32 arg0, SRT* arg1) {
+    Object* temp_v0_3;
+    CurveSetup* sp30;
+    f32 sp2C;
+    s32 temp_v0;
+    f32 xDist, zDist;
+
+    sp2C = 1000.0f;
+    temp_v0 = gDLL_26_Curves->vtbl->func_218C(arg0);
+    if (temp_v0 >= 0) {
+        sp30 = gDLL_26_Curves->vtbl->func_39C(temp_v0);
+        arg1->transl.x = sp30->pos.x;
+        arg1->transl.y = sp30->pos.y;
+        arg1->transl.z = sp30->pos.z;
+        temp_v0_3 = obj_get_nearest_type(OBJTYPE_9, &arg1->transl, &sp2C);
+        if (temp_v0_3 != NULL) {
+            xDist = temp_v0_3->srt.transl.x - arg1->transl.x;
+            zDist = temp_v0_3->srt.transl.z - arg1->transl.z;
+            arg1->yaw = (s16) (arctan2s(xDist, zDist) + 0x8000);
+        } else {
+            arg1->yaw = (s16) (sp30->unk2C << 8);
+        }
+        return 1;
+    }
+    return 0;
+}
 
 // offset: 0x1DEC | func: 17 | export: 8
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_1DEC.s")
+f32 dll_53_func_1DEC(Object* arg0, s32 arg1) {
+    s32 temp_v0 = gDLL_26_Curves->vtbl->func_218C(arg1);
+    if (temp_v0 >= 0) {
+        return gDLL_26_Curves->vtbl->func_174C(arg0, temp_v0);
+    }
+    return -1.0f;
+}
 
 // offset: 0x1E70 | func: 18 | export: 6
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_1E70.s")
+void dll_53_func_1E70(MoveLibData* arg0, s16* arg1, s16* arg2, s32 arg3) {
+    if (arg1 == NULL) {
+        arg1 = data_0;
+    }
+    if (arg2 == NULL) {
+        arg2 = data_0;
+    }
+    bcopy(arg1, &arg0->unk454, arg0->unk4A8 * 2);
+    bcopy(arg2, &arg0->unk454[15], arg0->unk4A8 * 2);
+}
 
 // offset: 0x1F1C | func: 19 | export: 12
+#ifndef NON_MATCHING
+s32 dll_53_func_1F1C(Object* arg0, s32 arg1, Vec3f* arg2);
 #pragma GLOBAL_ASM("asm/nonmatchings/dlls/engine/53_movelib/dll_53_func_1F1C.s")
+#else
+s32 dll_53_func_1F1C(Object* arg0, s32 arg1, Vec3f* arg2) {
+    Model* temp_t2;
+    ModelInstance* temp_t1;
+    ObjectHitInfo* temp_v0;
+    Vtx* temp_v1_3;
+    s32 var_v0;
+    s32 temp2;
+    MtxF* mtx;
+    ModelFacebatch* face;
+    s8 *new_var;
+
+    temp_v0 = arg0->objhitInfo;
+    temp_t1 = arg0->modelInsts[arg0->modelInstIdx];
+    temp_t2 = temp_t1->model;
+    if ((temp_v0 != NULL) && ((temp_v0->unk9F != 0) || (temp_v0->unk9E != 0))) {
+        return 0;
+    }
+    if (arg1 >= temp_t2->vertexCount) {
+        return 0;
+    }
+    // this is almost certainly a bug. should be: !(temp_t1->unk34 & 8)
+    temp2 = !temp_t1->unk34;
+    if (temp2 & 8) {
+        return 0;
+    }
+    temp_v1_3 = temp_t1->vertices[(temp_t1->unk34 >> 1) & 1];
+    var_v0 = 0;
+    while (var_v0 < temp_t2->unk70 && arg1 >= temp_t2->faces[var_v0 + 1].baseVertexID) {
+        var_v0 += 1;
+    }
+    if (var_v0 == temp_t2->unk70) {
+        return 0;
+    }
+    face = &temp_t2->faces[var_v0];
+    new_var = &face->jointID_A;
+    arg2->x = (f32) temp_v1_3[arg1].v.ob[0];
+    arg2->y = (f32) temp_v1_3[arg1].v.ob[1];
+    arg2->z = (f32) temp_v1_3[arg1].v.ob[2];
+    vec3_transform(&temp_t1->matrices[temp_t1->unk34 & 1][*new_var],
+        arg2->x, 
+        arg2->y, 
+        arg2->z, 
+        &arg2->x, 
+        &arg2->y, 
+        &arg2->z);
+    arg2->x += gWorldX;
+    arg2->z += gWorldZ;
+    return temp_t2->vertexCount;
+}
+#endif
 
 /*0x38*/ static const char str_38[] = "Got Curve ";
 /*0x44*/ static const char str_44[] = " PathId %i Loc id %i \n";
