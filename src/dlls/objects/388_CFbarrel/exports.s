dea4cb00d3413242745d25fc0b78e9ba61b92324
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2026-05-26 16:22:41 -0400

    forgor these

--- src/dlls/objects/388_CFbarrel/exports.s
+++ src/dlls/objects/388_CFbarrel/exports.s
@@ -4,15 +4,15 @@
 _exports:
 
 # ctor/dtor
-.dword dll_388_ctor
-.dword dll_388_dtor
+.dword CFbarrel_ctor
+.dword CFbarrel_dtor
 
 # export table
-/*0*/ .dword dll_388_setup
-/*1*/ .dword dll_388_control
-/*2*/ .dword dll_388_update
-/*3*/ .dword dll_388_print
-/*4*/ .dword dll_388_free
-/*5*/ .dword dll_388_get_model_flags
-/*6*/ .dword dll_388_get_data_size
-/*7*/ .dword dll_388_func_17F4
+/*0*/ .dword CFbarrel_setup
+/*1*/ .dword CFbarrel_control
+/*2*/ .dword CFbarrel_update
+/*3*/ .dword CFbarrel_print
+/*4*/ .dword CFbarrel_free
+/*5*/ .dword CFbarrel_get_model_flags
+/*6*/ .dword CFbarrel_get_data_size
+/*7*/ .dword CFbarrel_func_17F4
