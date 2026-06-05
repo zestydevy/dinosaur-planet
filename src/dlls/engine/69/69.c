777ff169f2d6014f9564e55b5dd49123aa000c72
EoinODoodles <e.odrisceoil@gmail.com>
2026-06-05 12:04:22 +0100

    Match: DLL 69

---
+++ src/dlls/engine/69/69.c
@@ -0,0 +1,22 @@
+#include "common.h"
+
+// offset: 0x0 | ctor
+void dll_69_ctor(void *dll) { }
+
+// offset: 0xC | dtor
+void dll_69_dtor(void *dll) { }
+
+// offset: 0x18 | func: 0 | export: 0
+s32 dll_69_update1(void) {
+    return 0;
+}
+
+// offset: 0x24 | func: 1 | export: 1
+void dll_69_update2(void) {
+
+}
+
+// offset: 0x2C | func: 2 | export: 2
+void dll_69_draw(Gfx** gdl, Mtx** mtx, Vertex** vtx) {
+
+}
