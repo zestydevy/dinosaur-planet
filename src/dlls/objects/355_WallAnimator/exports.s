239f829cb1590a7109edf28618cb4166bc8ccb0d
EoinODoodles <e.odrisceoil@gmail.com>
2026-06-10 13:22:51 +0100

    WallAnimator: cleanup and docs

--- src/dlls/objects/355_WallAnimator/exports.s
+++ src/dlls/objects/355_WallAnimator/exports.s
@@ -4,17 +4,17 @@
 _exports:
 
 # ctor/dtor
-.dword dll_355_ctor
-.dword dll_355_dtor
+.dword WallAnimator_ctor
+.dword WallAnimator_dtor
 
 # export table
-/*0*/ .dword dll_355_setup
-/*1*/ .dword dll_355_control
-/*2*/ .dword dll_355_update
-/*3*/ .dword dll_355_print
-/*4*/ .dword dll_355_free
-/*5*/ .dword dll_355_get_model_flags
-/*6*/ .dword dll_355_get_data_size
-/*7*/ .dword dll_355_func_B54
-/*8*/ .dword dll_355_func_C20
-/*9*/ .dword dll_355_func_C3C
+/*0*/ .dword WallAnimator_setup
+/*1*/ .dword WallAnimator_control
+/*2*/ .dword WallAnimator_update
+/*3*/ .dword WallAnimator_print
+/*4*/ .dword WallAnimator_free
+/*5*/ .dword WallAnimator_get_model_flags
+/*6*/ .dword WallAnimator_get_data_size
+/*7*/ .dword WallAnimator_tick_dig
+/*8*/ .dword WallAnimator_is_dig_finished
+/*9*/ .dword WallAnimator_get_energy_cost
--- src/dlls/objects/355_WallAnimator/wallanim.c
+++ src/dlls/objects/355_WallAnimator/wallanim.c
@@ -1,104 +1,107 @@
 #include "common.h"
 #include "dlls/objects/common/sidekick.h"
 #include "sys/map.h"
+#include "sys/math.h"
 #include "sys/memory.h"
 #include "sys/objtype.h"
 
 typedef struct {
-    ObjSetup base;
-    s16 gamebitA;
-    s16 _unk1A;
-    s16 unk1C;
-    u8 _unk1E;
-    u8 _unk1F;
-    u8 unk20;
-    u8 unk21;
-    s8 _unk22;
-    u8 unk23;
-    s16 yaw;        //NOTE: 16-bit precision, instead of setups' usual 8-bit yaw.
-    u8 unk26;
-} DLL355_Setup;
+/*00*/ ObjSetup base;
+/*18*/ s16 gamebitDug;          //Gamebit to set when Tricky digs through the wall
+/*1A*/ s16 sidekickExperience;  //Not used by the WallAnimator itself, but Tricky must have this much experience to dig the spot ("It's too deep, I need more practice!") 
+/*1C*/ s16 sidekickCost;        //Tricky must have this much energy to dig the spot ("I'm too tired to do this right now!")
+/*1E*/ s8 _unk1E;               //Unused - all existing WallAnimator objSetups have this set to 10
+/*1F*/ s8 _unk1F;               //Unused - all existing WallAnimator objSetups have this set to 0
+/*20*/ u8 findCommandRadius;    //Range for Find command to show up in inventory
+/*21*/ u8 falloffRadius;        //Tagged shapes' vertices will be influenced if they're inside this radius (relative to the vertex nearest the WallAnimator)
+/*22*/ s8 _unk22;               //Unused - all existing WallAnimator objSetups have this set to 60
+/*23*/ u8 animatorID;           //Block shapes with this tag will be animated
+/*24*/ s16 yaw;                 //NOTE: 16-bit precision, instead of setups' usual 8-bit yaw.
+/*26*/ u8 unk26;                //Stored to `objData->unk28` during setup, but otherwise unused (all existing WallAnimator objSetups have this set to 40)
+} WallAnimator_Setup;
 
 typedef struct {
-    f32 unk0;
-    f32 unk4;
-    f32 unk8;
-    f32 unkC;
-    f32* unk10;
-    Vec3f unk14;
-    s32 unk20;
-    f32 unk24;
-    f32 unk28;
-    s8 unk2C;
-    u8 animatorID; //animatorID
-    u8 unk2E;
-    u8 unk2F;
-    u8 unk30;
-    s8 unk31;
-    s16 gamebitDug;
-} DLL355_Data;
+    Vec3f digVector;            //The direction vertices will be extruded along while digging (2D only)
+    f32 zero;                   //Set to 0 during dig vector setup, but otherwise unused
+    f32* vtxWeights;            //Influence values (from 0 to 1) for each vertex being animated, falling off from a central vertex. Vertices with nonzero weighting are all extruded to the same extent however, since these values aren't multipled in. (NOTE: stores a float for each vertex in the animatorID tagged shapes, not just the few with nonzero influence!)
+    Vec3f closestVtxPosition;   //The position of the animatorID-tagged vertex closest to the WallAnimator, relative to the Block's origin. Used as a falloff centrepoint for influencing neighbouring vertices.
+    s32 digDepth;               //Current dig progress (starts at 0, increases while digging)
+    f32 falloffRadius;          //Tagged shapes' vertices will be influenced if they're inside this radius (relative to the vertex nearest the WallAnimator)
+    f32 unk28;                  //Stores `objSetup->unk26` during setup, but otherwise unused
+    s8 previousDigDepth;        //Previous dig progress value (vertex updates are queued when this differs from current dig value)
+    u8 animatorID;              //Block shapes with this tag will be animated
+    u8 sidekickCost;            //Tricky must have this much energy to dig the spot ("I'm too tired to do this right now!")
+    u8 animUpdates;             //Used to queue vertex animation updates
+    u8 digFinished;             //Boolean set when Tricky's finished digging through the wall
+    s16 gamebitDug;             //Gamebit to set when Tricky digs through the wall
+} WallAnimator_Data;
 
+#define DIG_DEPTH_MAX 3000
 #define OPACITY_MAX 0xFF
 
-static void dll_355_func_7F4(Object* self, DLL355_Data* objData);
+static void WallAnimator_calculate_vertex_weights(Object* self, WallAnimator_Data* objData);
 
 // offset: 0x0 | ctor
-void dll_355_ctor(void *dll) { }
+void WallAnimator_ctor(void *dll) { }
 
 // offset: 0xC | dtor
-void dll_355_dtor(void *dll) { }
+void WallAnimator_dtor(void *dll) { }
 
 // offset: 0x18 | func: 0 | export: 0
-void dll_355_setup(Object* self, DLL355_Setup* objSetup, s32 reset) {
+void WallAnimator_setup(Object* self, WallAnimator_Setup* objSetup, s32 reset) {
     s32 pad[2];
-    s32 animatedVtxCount; //2C
-    DLL355_Data* objData;
+    s32 animatedVtxCount;
+    WallAnimator_Data* objData;
 
     objData = self->data;
     
     self->srt.yaw = objSetup->yaw;
     
-    objData->animatorID = objSetup->unk23;
-    objData->unk2E = objSetup->unk1C;
-    objData->unk20 = 0;
-    objData->unk2C = -1;
-
-    objData->unk24 = objSetup->unk21;
-    objData->gamebitDug = objSetup->gamebitA;
+    objData->animatorID = objSetup->animatorID;
+    objData->sidekickCost = objSetup->sidekickCost;
+    objData->digDepth = 0;
+    objData->previousDigDepth = -1;
+    objData->falloffRadius = objSetup->falloffRadius;
+    objData->gamebitDug = objSetup->gamebitDug;
     objData->unk28 = objSetup->unk26;
     
-
+    //Check if the local Block has any vertices with the given animatorID
     animatedVtxCount = block_get_animator_vertex_count(self, objData->animatorID);
     if (animatedVtxCount == 0) {
         objData->animatorID = 0;
     }
-
-    if (objData->animatorID != 0) {
-        objData->unk0 = fsin16_precise(self->srt.yaw);
-        objData->unk4 = 0.0f;
-        objData->unk8 = fcos16_precise(self->srt.yaw);
-        objData->unkC = 0.0f;
-        
-        if (reset == 0) {
-            objData->unk10 = mmAlloc(animatedVtxCount * sizeof(f32), ALLOC_TAG_TRACK_COL, NULL);
-            dll_355_func_7F4(self, objData);
-        }
-        
-        if (main_get_bits(objSetup->gamebitA)) {
-            objData->unk30 = 1;
-            objData->unk20 = 3000;
-        }
-        
-        obj_add_object_type(self, OBJTYPE_TrickyTarget);
-        obj_add_object_type(self, OBJTYPE_WallAnimator);
+    if (objData->animatorID == 0){
+        return;
     }
+
+    //Calculate the dig vector
+    objData->digVector.x = fsin16_precise(self->srt.yaw);
+    objData->digVector.y = 0.0f;
+    objData->digVector.z = fcos16_precise(self->srt.yaw);
+    
+    objData->zero = 0.0f;
+    
+    //Set up vertex weights
+    if (reset == FALSE) {
+        objData->vtxWeights = mmAlloc(animatedVtxCount * sizeof(f32), ALLOC_TAG_TRACK_COL, NULL);
+        WallAnimator_calculate_vertex_weights(self, objData);
+    }
+    
+    //Check if the wall's already been dug through
+    if (main_get_bits(objSetup->gamebitDug)) {
+        objData->digFinished = TRUE;
+        objData->digDepth = DIG_DEPTH_MAX;
+    }
+    
+    obj_add_object_type(self, OBJTYPE_TrickyTarget);
+    obj_add_object_type(self, OBJTYPE_WallAnimator);
 }
 
 // offset: 0x1E8 | func: 1 | export: 1
-void dll_355_control(Object* self) {
-    DLL355_Data* objData;
+void WallAnimator_control(Object* self) {
+    WallAnimator_Data* objData;
     Block* block;
-    DLL355_Setup* objSetup;
+    WallAnimator_Setup* objSetup;
     BlockVertex *vertices;
     BlockShape* shapes;
     s32 shapeIdx;
@@ -113,25 +116,30 @@ void dll_355_control(Object* self) {
 
     objData = self->data;
     
+    //Return early if the dig spot doesn't animate anything
     if (objData->animatorID == 0) {
         return;
     }
+
+    objSetup = (WallAnimator_Setup*)self->setup;
     
-    objSetup = (DLL355_Setup*)self->setup;
-    if (objData->unk20 != 3000) {
+    //Show the Find command when the player is nearby (provided there isn't a nearby Pushblock obstructing the dig spot)
+    if (objData->digDepth != DIG_DEPTH_MAX) {
         player = get_player();
         sidekick = get_sidekick();
         distance = M_INFINITY_F;
         pushblock = obj_get_nearest_type_to(OBJTYPE_PushBlock, self, &distance);
+
         if (sidekick && ((pushblock == NULL) || (pushblock && (distance > 35.0f)))) {
             distance = vec3_distance_squared(&self->globalPosition, &player->globalPosition);
-            if (distance <= SQ(objSetup->unk20)) {
+            if (distance <= SQ(objSetup->findCommandRadius)) {
                 ((DLL_ISidekick*)sidekick->dll)->vtbl->func14(sidekick, Sidekick_Command_INDEX_1_Find);
             }
         }
     }
     
-    if (objData->unk20 == 0) {
+    //Return if the digging hasn't started yet
+    if (objData->digDepth == 0) {
         return;
     }
     
@@ -143,23 +151,27 @@ void dll_355_control(Object* self) {
         return;
     }
     
-    if (objData->unk20 != objData->unk2C) {
-        objData->unk2F = 2;
-        objData->unk2C = objData->unk20;
+     //Tick animation for 2 frames (including the current one) whenever the dig depth changes
+    if (objData->digDepth != objData->previousDigDepth) {
+        objData->animUpdates = 2;
+        objData->previousDigDepth = objData->digDepth;
     }
-    
-    if (objData->unk2F == 0) {
+    if (objData->animUpdates == 0) {
         return;
     }
-    objData->unk2F--;
+    objData->animUpdates--;
     
-    if ((objData->unk20 >= 3000) && (objData->unk30 == 0)) {
-        objData->unk30 = 1;
-        objData->unk20 = 3000;
+    //Handle when digging is finished
+    if ((objData->digDepth >= DIG_DEPTH_MAX) && (objData->digFinished == FALSE)) {
+        objData->digFinished = TRUE;
+        objData->digDepth = DIG_DEPTH_MAX;
+
         main_set_bits(objData->gamebitDug, TRUE);
+
         gDLL_6_AMSFX->vtbl->play(self, SOUND_B01_Success_Chime, MAX_VOLUME, NULL, NULL, 0, NULL);
     }
 
+    //Animate vertices
     vertices = (BlockVertex*)block->vertices2[block->vtxFlags & 1]; 
     shapeIdx = 0;
     animVtxIdx = 0;
@@ -169,25 +181,25 @@ void dll_355_control(Object* self) {
 
             for (vertexIdx = shapes[shapeIdx].vtxBase; vertexIdx < shapes[shapeIdx + 1].vtxBase; vertexIdx++, animVtxIdx++) {
                 
-                if (objData->unk10[animVtxIdx] > 0.0f) {
-                    displacement = (objData->unk20 / 100.0f);
+                if (objData->vtxWeights[animVtxIdx] > 0.0f) {
+                    displacement = (objData->digDepth / 100.0f); //NOTE: the vtxWeight isn't multiplied in here, unlike GroundAnimators
                     
                     //Extrude vertices
                     if ((shapes[shapeIdx].flags & RENDER_UNK20000000)) {
-                        vertices[vertexIdx].ob[0] = block->vertices[vertexIdx].ob[0] + displacement*objData->unk0;
-                        vertices[vertexIdx].ob[2] = block->vertices[vertexIdx].ob[2] + displacement*objData->unk8;
+                        vertices[vertexIdx].ob[0] = block->vertices[vertexIdx].ob[0] + displacement*objData->digVector.x;
+                        vertices[vertexIdx].ob[2] = block->vertices[vertexIdx].ob[2] + displacement*objData->digVector.z;
                     } else {
-                        vertices[vertexIdx].ob[0] = block->vertices[vertexIdx].ob[0] + displacement*objData->unk0;
-                        vertices[vertexIdx].ob[2] = block->vertices[vertexIdx].ob[2] + displacement*objData->unk8;
+                        vertices[vertexIdx].ob[0] = block->vertices[vertexIdx].ob[0] + displacement*objData->digVector.x;
+                        vertices[vertexIdx].ob[2] = block->vertices[vertexIdx].ob[2] + displacement*objData->digVector.z;
                     }
                     
                     //Animate vertex colours
                     displacement = displacement / 30;
-                    fadeOut = displacement * 255;
-                    vertices[vertexIdx].cn[0] = (block->vertices[vertexIdx].cn[0] * (0xFF - fadeOut)) >> 8;
-                    vertices[vertexIdx].cn[1] = (block->vertices[vertexIdx].cn[1] * (0xFF - fadeOut)) >> 8;
-                    vertices[vertexIdx].cn[2] = (block->vertices[vertexIdx].cn[2] * (0xFF - fadeOut)) >> 8;
-                    vertices[vertexIdx].cn[3] = 0xFF - fadeOut;
+                    fadeOut = displacement * OPACITY_MAX;
+                    vertices[vertexIdx].cn[0] = (block->vertices[vertexIdx].cn[0] * (OPACITY_MAX - fadeOut)) >> 8;
+                    vertices[vertexIdx].cn[1] = (block->vertices[vertexIdx].cn[1] * (OPACITY_MAX - fadeOut)) >> 8;
+                    vertices[vertexIdx].cn[2] = (block->vertices[vertexIdx].cn[2] * (OPACITY_MAX - fadeOut)) >> 8;
+                    vertices[vertexIdx].cn[3] = OPACITY_MAX - fadeOut;
                 }
             }
         }
@@ -195,21 +207,21 @@ void dll_355_control(Object* self) {
 }
 
 // offset: 0x6E8 | func: 2 | export: 2
-void dll_355_update(Object *self) { }
+void WallAnimator_update(Object *self) { }
 
 // offset: 0x6F4 | func: 3 | export: 3
-void dll_355_print(Object* self, Gfx** gdl, Mtx** mtxs, Vertex** vtxs, Triangle** pols, s8 visibility) {
+void WallAnimator_print(Object* self, Gfx** gdl, Mtx** mtxs, Vertex** vtxs, Triangle** pols, s8 visibility) {
     if (visibility) {
         draw_object(self, gdl, mtxs, vtxs, pols, 1.0f);
     }
 }
 
 // offset: 0x748 | func: 4 | export: 4
-void dll_355_free(Object* self, s32 onlySelf) {
-    DLL355_Data* objData = self->data;
+void WallAnimator_free(Object* self, s32 onlySelf) {
+    WallAnimator_Data* objData = self->data;
 
-    if (objData->unk10 != NULL) {
-        mmFree(objData->unk10);
+    if (objData->vtxWeights != NULL) {
+        mmFree(objData->vtxWeights);
     }
     
     obj_free_object_type(self, OBJTYPE_TrickyTarget);
@@ -217,17 +229,17 @@ void dll_355_free(Object* self, s32 onlySelf) {
 }
 
 // offset: 0x7D0 | func: 5 | export: 5
-u32 dll_355_get_model_flags(Object *self) {
+u32 WallAnimator_get_model_flags(Object *self) {
     return MODFLAGS_NONE;
 }
 
 // offset: 0x7E0 | func: 6 | export: 6
-u32 dll_355_get_data_size(Object *self, u32 offsetAddr) {
-    return sizeof(DLL355_Data);
+u32 WallAnimator_get_data_size(Object *self, u32 offsetAddr) {
+    return sizeof(WallAnimator_Data);
 }
 
 // offset: 0x7F4 | func: 7
-void dll_355_func_7F4(Object* self, DLL355_Data* objData) {
+void WallAnimator_calculate_vertex_weights(Object* self, WallAnimator_Data* objData) {
     BlockShape* shapes;
     f32 distanceSquared;
     f32 dy;
@@ -237,7 +249,7 @@ void dll_355_func_7F4(Object* self, DLL355_Data* objData) {
     f32 dz;
     f32 tValue;
     f32 minDistance;
-    s32 closestVtxBase;
+    s32 closestVtxID;
     s32 shapeIdx;
     s32 blockWorldGridX;
     s32 blockWorldGridZ;
@@ -246,23 +258,25 @@ void dll_355_func_7F4(Object* self, DLL355_Data* objData) {
     s32 animVtxIdx;
     f32 radiusSq;
 
+    //Get local Block and make sure its vertices are animatable
     block = map_get_block_by_index(map_world_coords_to_block_index(
         self->srt.transl.x, self->srt.transl.y, self->srt.transl.z)
     );
-    
     if ((block == NULL) || !(block->vtxFlags & 8)) {
         return;
     }
     
+    //Get the WallAnimator's position relative to the Blocks model's local origin
     blockWorldGridX = floor_f((self->srt.transl.x - gWorldX) / BLOCKS_GRID_UNIT_F);
     blockWorldGridZ = floor_f((self->srt.transl.z - gWorldZ) / BLOCKS_GRID_UNIT_F);
     
     objBlockX = self->srt.transl.x - (blockWorldGridX * BLOCKS_GRID_UNIT_F + gWorldX);
     objBlockZ = self->srt.transl.z - (blockWorldGridZ * BLOCKS_GRID_UNIT_F + gWorldZ);
     
+    //Find the vertexID closest to the WallAnimator
     minDistance = 10000.0f;
     shapeIdx = 0;
-    closestVtxBase = -1;
+    closestVtxID = -1;
     shapes = block->shapes;
     for (; shapeIdx < block->shapeCount; shapeIdx++) {
         if (objData->animatorID == shapes[shapeIdx].animatorID) {
@@ -275,32 +289,36 @@ void dll_355_func_7F4(Object* self, DLL355_Data* objData) {
                 distanceSquared = SQ(dx) + SQ(dz);
                 if (distanceSquared < minDistance) {
                     minDistance = distanceSquared;
-                    closestVtxBase = vtxID;
+                    closestVtxID = vtxID;
                 }
             }
         }
     }
 
-    tValue = 1.0f; //fake?
-    
-    objData->unk14.f[0] = block->vertices[closestVtxBase].ob[0];
-    objData->unk14.f[1] = block->vertices[closestVtxBase].ob[1];
-    objData->unk14.f[2] = block->vertices[closestVtxBase].ob[2];
+    //Store the closest vertex's coords
+    objData->closestVtxPosition.x = block->vertices[closestVtxID].ob[0];
+    objData->closestVtxPosition.y = block->vertices[closestVtxID].ob[1];
+    objData->closestVtxPosition.z = block->vertices[closestVtxID].ob[2];
 
+    //Search through the Block's shapes, looking for any tagged with the animatorID
+    tValue = 1.0f;
     shapes = block->shapes;
     for (shapeIdx = 0, animVtxIdx = 0; shapeIdx < block->shapeCount; shapeIdx++) {
+        //Only animate shapes that have a matching animatorID
         if (objData->animatorID == shapes[shapeIdx].animatorID) {
+
+            //Calculate vertices' influence weights, based on falloff and distance from central vertex
             for (vtxID = shapes[shapeIdx].vtxBase; vtxID < shapes[shapeIdx + 1].vtxBase; vtxID++, animVtxIdx++) {
                 dx = block->vertices[vtxID].ob[0];
                 dy = block->vertices[vtxID].ob[1];
                 dz = block->vertices[vtxID].ob[2];
                 
-                dx -= objData->unk14.x;
-                dy -= objData->unk14.y;
-                dz -= objData->unk14.z;
+                dx -= objData->closestVtxPosition.x;
+                dy -= objData->closestVtxPosition.y;
+                dz -= objData->closestVtxPosition.z;
 
                 tValue = (SQ(dx) + SQ(dy) + SQ(dz));
-                radiusSq = SQ(objData->unk24);
+                radiusSq = SQ(objData->falloffRadius);
                 tValue /= radiusSq;
                 if (tValue > 1.0f) {
                     tValue = 1.0f;
@@ -308,44 +326,49 @@ void dll_355_func_7F4(Object* self, DLL355_Data* objData) {
 
                 tValue = SQ(tValue);
                 tValue = 1.0f - tValue;
-                objData->unk10[animVtxIdx] = tValue;
+                objData->vtxWeights[animVtxIdx] = tValue;
             }
         }
     }
 }
 
 // offset: 0xB54 | func: 8 | export: 7
-f32 dll_355_func_B54(Object* self, Object* arg1) {
-    DLL355_Data* objData;
+f32 WallAnimator_tick_dig(Object* self, Object* sidekick) {
+    WallAnimator_Data* objData;
     f32 dy;
     f32 dx;
     f32 dz;
 
     objData = self->data;
     
-    dy = arg1->srt.transl.y - self->srt.transl.y;
+    //Return 0 if sidekick's too far above/below the dig spot
+    dy = sidekick->srt.transl.y - self->srt.transl.y;
     if ((dy < -20.0f) || (dy > 20.0f)) {
-        return 0.0f;
+        return 0;
     }
     
-    dx = arg1->srt.transl.x - self->srt.transl.x;
-    dz = arg1->srt.transl.z - self->srt.transl.z;
+    //Return 0 if the sidekick's not within lateral range
+    dx = sidekick->srt.transl.x - self->srt.transl.x;
+    dz = sidekick->srt.transl.z - self->srt.transl.z;
     if ((SQ(dx) + SQ(dz)) > 2500.0f) {
-        return 0.0f;
+        return 0;
     }
     
-    objData->unk20 += 60;
-    return (f32) objData->unk20 / 3000.0f;
+    //Advance digging
+    objData->digDepth += 60; //@framerate-dependent?
+
+    //Return a distance for the sidekick, based on the dig progress
+    return ((f32)objData->digDepth / DIG_DEPTH_MAX);
 }
 
 // offset: 0xC20 | func: 9 | export: 8
-u8 dll_355_func_C20(Object* self) {
-    DLL355_Data* objData = self->data;
-    return (objData->unk20 >= 3000);
+u8 WallAnimator_is_dig_finished(Object* self) {
+    WallAnimator_Data* objData = self->data;
+    return (objData->digDepth >= DIG_DEPTH_MAX);
 }
 
 // offset: 0xC3C | func: 10 | export: 9
-u8 dll_355_func_C3C(Object* self) {
-    DLL355_Data* objData = self->data;
-    return objData->unk2E;
+u8 WallAnimator_get_energy_cost(Object* self) {
+    WallAnimator_Data* objData = self->data;
+    return objData->sidekickCost;
 }
