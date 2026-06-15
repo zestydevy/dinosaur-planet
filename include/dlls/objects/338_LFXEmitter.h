fefee969be8a22605ba3d1ffb5038bff6c4212dd
EoinODoodles <e.odrisceoil@gmail.com>
2026-06-15 10:13:54 +0100

    LFXEmitter-related cleanup/interface

--- include/dlls/objects/338_LFXEmitter.h
+++ include/dlls/objects/338_LFXEmitter.h
@@ -11,16 +11,23 @@ typedef struct {
 } LFXEmitter_Data;
 
 typedef struct {
-    ObjSetup base;
-    s16 unk18;
-    s16 unk1A;
-    s16 unk1C;
-    s16 unk1E;
-    s16 unk20;
-    s16 unk22;
-    s8 unk24;
-    s8 unk25;
-    s16 unk26;
+/*00*/    ObjSetup base;
+/*18*/    s16 unk18;
+/*1A*/    s16 unk1A;
+/*1C*/    s16 unk1C;
+/*1E*/    s16 unk1E;
+/*20*/    s16 unk20;
+/*22*/    s16 unk22;
+/*24*/    s8 unk24;
+/*25*/    s8 unk25;
+/*26*/    s16 unk26;
 } LFXEmitter_Setup;
+
+DLL_INTERFACE(DLL_338_LFXEmitter) {
+	/*:*/ DLL_INTERFACE_BASE(DLL_IObject);
+    /*7*/ UnknownDLLFunc func7;
+    /*8*/ void (*func8)(Object* self, f32 opacity);
+    //TODO: find out how many other exports this DLL has
+};
 
 #endif // _DLLS_338_H
