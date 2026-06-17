01d1e2db63cc65cda8d147c35ae5b317d105668c
EoinODoodles <e.odrisceoil@gmail.com>
2026-06-17 19:16:50 +0100

    DLL 81: adding "swaphollow" name and MusicAction enum entries from kiosk

--- src/dlls/engine/81_swaphollow/swaphollow.c
+++ src/dlls/engine/81_swaphollow/swaphollow.c
@@ -1,6 +1,17 @@
 #include "common.h"
 #include "sys/segment_1460.h"
 
+/* MusicAction enum names for this DLL, from `default.dol`. */
+  typedef enum {
+    MFX_SH_TalkSwapstoneTune = 5,
+    MFX_SH_MushroomTune = 4,
+    MFX_SH_AboveGroundFullTune = 1,
+    MFX_SH_InHollowTune = 12,
+    MFX_SH_AmbienceNight = 22,
+    MFX_SH_AmbienceDay = 21,
+    MFX_SH_AboveGroundLowTune = 9 
+} SwapHollowDLLMusicActions;
+
 // offset: 0x0 | ctor
 void dll_81_ctor(void* dll) { }
 
@@ -15,6 +26,7 @@ void dll_81_func_18(Object* objA, Object* objB, s8 arg2, s32 arg3) {
         main_set_bits(BIT_351, FALSE);
     }
 }
+
 // offset: 0xA4 | func: 1 | export: 1
 void dll_81_func_A4(Object* objA, Object* objB, s8 arg2, s32 arg3) {
     f32 time;
@@ -24,14 +36,14 @@ void dll_81_func_A4(Object* objA, Object* objB, s8 arg2, s32 arg3) {
 
     if (arg2 == 1) {
         func_80000860(objA, objB, 0x142, arg3);
-        gDLL_5_AMSEQ2->vtbl->set(NULL, 5, 0, 0, 0);
+        gDLL_5_AMSEQ2->vtbl->set(NULL, MFX_SH_TalkSwapstoneTune, 0, 0, 0);
         main_set_bits(BIT_Played_Seq_01FD_Rocky_Teaches_Distract, TRUE);
     } else if (arg2 == -1) {
         func_80000860(objA, objB, 0x60, arg3);
-        if (isNighttime != 0) {
-            gDLL_5_AMSEQ2->vtbl->set(NULL, 4, 0, 0, 0);
+        if (isNighttime) {
+            gDLL_5_AMSEQ2->vtbl->set(NULL, MFX_SH_MushroomTune, 0, 0, 0);
         } else {
-            gDLL_5_AMSEQ2->vtbl->set(NULL, 1, 0, 0, 0);
+            gDLL_5_AMSEQ2->vtbl->set(NULL, MFX_SH_AboveGroundFullTune, 0, 0, 0);
         }
         main_set_bits(BIT_Played_Seq_01FD_Rocky_Teaches_Distract, FALSE);
     }
@@ -45,15 +57,15 @@ void dll_81_func_224(s32 arg0, Object* arg1, s8 arg2, s32 arg3) {
     isNighttime = gDLL_7_Newday->vtbl->func8(&time);
     
     if (arg2 == 1) {
-        gDLL_5_AMSEQ2->vtbl->set(NULL, 0xC, 0, 0, 0);
+        gDLL_5_AMSEQ2->vtbl->set(NULL, MFX_SH_InHollowTune, 0, 0, 0);
         main_set_bits(BIT_177, TRUE);
     } else if (arg2 == -1) {
         if (isNighttime) {
-            gDLL_5_AMSEQ2->vtbl->set(NULL, 4, 0, 0, 0);
-            gDLL_5_AMSEQ2->vtbl->set(NULL, 0x16, 0, 0, 0);
+            gDLL_5_AMSEQ2->vtbl->set(NULL, MFX_SH_MushroomTune, 0, 0, 0);
+            gDLL_5_AMSEQ2->vtbl->set(NULL, MFX_SH_AmbienceNight, 0, 0, 0);
         } else {
-            gDLL_5_AMSEQ2->vtbl->set(NULL, 1, 0, 0, 0);
-            gDLL_5_AMSEQ2->vtbl->set(NULL, 0x15, 0, 0, 0);
+            gDLL_5_AMSEQ2->vtbl->set(NULL, MFX_SH_AboveGroundFullTune, 0, 0, 0);
+            gDLL_5_AMSEQ2->vtbl->set(NULL, MFX_SH_AmbienceDay, 0, 0, 0);
         }
         main_set_bits(BIT_177, FALSE);
     }
@@ -67,12 +79,12 @@ void dll_81_func_3C8(Object* objA, Object* objB, s8 arg2, s32 arg3) {
     isNighttime = gDLL_7_Newday->vtbl->func8(&time);
     
     if (arg2 == 1) {
-        gDLL_5_AMSEQ2->vtbl->set(NULL, 9, 0, 0, 0);
+        gDLL_5_AMSEQ2->vtbl->set(NULL, MFX_SH_AboveGroundLowTune, 0, 0, 0);
     } else if (arg2 == -1) {
         if (isNighttime) {
-            gDLL_5_AMSEQ2->vtbl->set(NULL, 4, 0, 0, 0);
+            gDLL_5_AMSEQ2->vtbl->set(NULL, MFX_SH_MushroomTune, 0, 0, 0);
         } else {
-            gDLL_5_AMSEQ2->vtbl->set(NULL, 1, 0, 0, 0);
+            gDLL_5_AMSEQ2->vtbl->set(NULL, MFX_SH_AboveGroundFullTune, 0, 0, 0);
         }
     }
 }
