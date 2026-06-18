c54782f5d31d027a4c7b81f655541de3c1e833e9
EoinODoodles <e.odrisceoil@gmail.com>
2026-06-18 20:05:29 +0100

    Match: DLL 422 DFpulley

--- src/dlls/objects/421_DFcradle/DFcradle.c
+++ src/dlls/objects/421_DFcradle/DFcradle.c
@@ -2,49 +2,7 @@
 #include "sys/curves.h"
 #include "sys/objtype.h"
 #include "dlls/objects/347_texscroll2.h"
-
-typedef struct {
-    /* 0000 */ f32 unk0;
-    /* 0004 */ f32 unk4; // curveProgress? (lerp t-value from 0 to 100?)
-    /* 0008 */ f32 unk8;
-    /* 000C */ f32 unkC;
-    /* 0010 */ s32 unk10;
-    /* 0014 */ f32 unk14[20];
-    /* 0064 */ f32 unk64;
-    /* 0068 */ Vec3f unk68; //curve point (at current tValue)
-    /* 0074 */ f32 unk74; //curve tangent x (at current tValue) (TODO: Vec3f)
-    /* 0078 */ f32 unk78; //curve tangent y
-    /* 007C */ f32 unk7C; //curve tangent z 
-    /* 0080 */ s32 unk80;
-    /* 0084 */ f32* unk84;
-    /* 0088 */ f32* unk88;
-    /* 008C */ f32* unk8C;
-    /* 0090 */ s32 numControlPoints;
-    /* 0094 */ SplineFunc splineFunc;
-    /* 0098 */ SplineConverterFunc splineConverterFunc;
-} UnkCurvesStruct_Split;
-
-typedef struct {
-    ObjSetup base;
-    u8 roll;
-    u8 pitch;
-    u8 yaw;
-} DFCradle_Setup;
-
-typedef struct {
-    UnkCurvesStruct_Split curves;
-    f32 unk9C;          //Spline tValue?
-    f32* splineX;
-    f32* splineY;
-    f32* splineZ;
-    f32 pulleyValLower; //Related to the rotating middle pulley closer to SwapStone Circle entrance
-    f32 pulleyValUpper; //Related to the rotating middle pulley closer to the shrine entrance
-    s8 enabled;         //Cradle has been powered (Kyte activated turbine lever)
-    s8 direction;       //1 or -1
-    u8 unkB6;
-    u8 prevCradleVal;
-    s8 soundTimer; //Randomised interval between rope straining sounds
-} DFCradle_Data;
+#include "dlls/objects/421_DFcradle.h"
 
 /*0x0*/ static s16 sSoundIDs[] = {
     SOUND_783_Rope_Strain, 
