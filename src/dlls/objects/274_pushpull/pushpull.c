33604e125ca75f4bff0c8a225525b49defcfd4eb
EoinODoodles <e.odrisceoil@gmail.com>
2026-07-08 12:42:23 +0100

    DLL 274 pushpull progress

--- src/dlls/objects/274_pushpull/pushpull.c
+++ src/dlls/objects/274_pushpull/pushpull.c
@@ -4,14 +4,34 @@
 #include "dlls/engine/6_amsfx.h"
 #include "game/gamebits.h"
 #include "game/objects/object.h"
+#include "game/objects/object_id.h"
 #include "sys/main.h"
+#include "sys/objects.h"
+#include "sys/objmsg.h"
+#include "sys/objtype.h"
 #include "types.h"
 
 typedef struct {
-    u8 _unk0[0xC8 - 0x0];
+    ObjSetup base;
+    s16 unk18;
+} DLL274_Setup;
+
+typedef struct {
+    u8 _unk0[0xC - 0x0];
+    f32 unkC;
+    u8 _unk10[0x78 - 0x10];
+    u32 unk78;
+    u32 unk7C;
+    s16 unk80;
+    u8 _unk82[0x8C - 0x82];
+    Object* unk8C;
+    u8 _unk90[0xC4 - 0x90];
+    f32 unkC4;
     f32 unkC8;
     f32 unkCC;
-    u8 _unkD0[0xDC - 0xD0];
+    u8 _unkD0[0xD4 - 0xD0];
+    u16 unkD4;
+    u8 _unkD6[0xDC - 0xD6];
 } DLL274_Data;
 
 /*0x0*/ static u32 _data_0[] = {
@@ -50,8 +70,34 @@ void dll_274_print(Object* self, Gfx** gdl, Mtx** mtxs, Vertex** vtxs, Triangle*
 #pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/274_pushpull/dll_274_print.s")
 
 // offset: 0x1058 | func: 4 | export: 4
-void dll_274_free(Object *self, s32 a1);
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/274_pushpull/dll_274_free.s")
+void dll_274_free(Object* self, s32 onlySelf) {
+    DLL274_Data* objData;
+    DLL274_Setup* objSetup;
+
+    objSetup = (DLL274_Setup*)self->setup;
+    objData = self->data;
+    
+    switch (self->id) {
+    case OBJ_NWSH_colpush:
+        main_set_bits(objData->unk80, 0);
+        break;
+    case OBJ_NWSH_colpushped:
+        main_set_bits(objData->unk80, 0);
+        break;
+    default:
+        if (objSetup->unk18 >= 0) {
+            map_save_object(&objSetup->base, self->mapID, objSetup->base.x, objSetup->base.y, objSetup->base.z);
+        }
+        break;
+    }
+
+    if (objData->unk78) {
+        gDLL_6_AMSFX->vtbl->stop(objData->unk78);
+        objData->unk78 = 0;
+    }
+    
+    obj_free_object_type(self, OBJTYPE_PushBlock);
+}
 
 // offset: 0x116C | func: 5 | export: 5
 u32 dll_274_get_model_flags(Object* self);
@@ -66,19 +112,79 @@ u32 dll_274_get_data_size(Object *self, u32 a1) {
 #pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/274_pushpull/dll_274_func_1190.s")
 
 // offset: 0x176C | func: 8 | export: 8
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/274_pushpull/dll_274_func_176C.s")
+s32 dll_274_func_176C(Object* self, Object* obj) {
+    Vec3f d;
+    s32 pad2;
+    DLL274_Data* objData;
+    s32 pad1;
+
+    objData = self->data;
+    
+    d.f[0] = obj->srt.transl.x - self->srt.transl.x;
+    d.f[1] = obj->srt.transl.y - self->srt.transl.y;
+    d.f[2] = obj->srt.transl.z - self->srt.transl.z;
+
+    return (sqrtf(SQ(d.f[0]) + SQ(d.f[1]) + SQ(d.f[2])) < objData->unkC);
+}
 
 // offset: 0x1814 | func: 9 | export: 9
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/274_pushpull/dll_274_func_1814.s")
+void dll_274_func_1814(Object* self, s32 bitIndex) {
+    DLL274_Data* objData = self->data;
+    objData->unk7C |= (1 << bitIndex);
+}
 
 // offset: 0x1834 | func: 10 | export: 10
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/274_pushpull/dll_274_func_1834.s")
+s32 dll_274_func_1834(Object* self) {
+    DLL274_Data* objData = self->data;
+    return objData->unkD4 & 1;
+}
 
 // offset: 0x184C | func: 11
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/274_pushpull/dll_274_func_184C.s")
+s32 dll_274_func_184C(Object* self, s32 arg1, f32 arg2, f32* arg3) {
+    UnkFunc_80024108Struct sp24;
+    s32 temp_v1;
+
+    sp24.unk12 = 0;
+    temp_v1 = func_80024108(self, arg2, 1.0f, &sp24);
+    if (sp24.unk12 != 0) {
+        *arg3 = sp24.unk0[2];
+    } else {
+        *arg3 = 0.0f;
+    }
+    
+    return temp_v1;
+}
 
 // offset: 0x18C8 | func: 12
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/274_pushpull/dll_274_func_18C8.s")
+void dll_274_func_18C8(Object* self, s32 arg1) {
+    DLL274_Data* objData;
+    Object* outSender;
+    u32 outMessageID;
+    f32* outMesgArg;
+
+    objData = self->data;
+    outMesgArg = NULL;
+    while (obj_recv_mesg(self, &outMessageID, &outSender, (void*)&outMesgArg)) {
+        switch (outMessageID) {
+        case 0xF0003:
+            objData->unk8C = outSender;
+            break;
+        case 0xE:
+            if ((self->id != OBJ_NWSH_colpush) && (self->id != OBJ_NWSH_colpushped)) {
+                obj_destroy_object(self);
+            }
+            break;
+        case 0x40001:
+            if (self->id == OBJ_NWSH_colpush) {
+                objData->unkC4 = *outMesgArg;
+            }
+            if (self->id == OBJ_NWSH_colpushped) {
+                objData->unkC4 = *outMesgArg;
+            }
+            break;
+        }
+    }
+}
 
 // offset: 0x1A24 | func: 13
 #pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/274_pushpull/dll_274_func_1A24.s")
@@ -87,7 +193,19 @@ u32 dll_274_get_data_size(Object *self, u32 a1) {
 #pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/274_pushpull/dll_274_func_1D18.s")
 
 // offset: 0x2020 | func: 15
-#pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/274_pushpull/dll_274_func_2020.s")
+void dll_274_func_2020(Object* self, MtxF* oMtx) {
+    SRT srt;
+
+    srt.yaw = self->srt.yaw;
+    srt.pitch = self->srt.pitch;
+    srt.roll = self->srt.roll;
+    srt.scale = 1.0f;
+    srt.transl.x = self->srt.transl.x;
+    srt.transl.y = self->srt.transl.y;
+    srt.transl.z = self->srt.transl.z;
+    
+    matrix_from_srt(oMtx, &srt);
+}
 
 // offset: 0x20A0 | func: 16
 #pragma GLOBAL_ASM("asm/nonmatchings/dlls/objects/274_pushpull/dll_274_func_20A0.s")
