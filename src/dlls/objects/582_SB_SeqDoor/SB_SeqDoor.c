82b15bd1b8a4d62b56be45112e40b13da1920b75
Banjeoin <e.odrisceoil@gmail.com>
2026-07-15 19:48:15 +0100

    SB_SeqDoor: small cleanup

--- src/dlls/objects/582_SB_SeqDoor/SB_SeqDoor.c
+++ src/dlls/objects/582_SB_SeqDoor/SB_SeqDoor.c
@@ -1,13 +1,14 @@
+#include "game/objects/interaction_arrow.h"
 #include "sys/gfx/animseq.h"
 #include "sys/objprint.h"
 
 typedef struct {
 /*00*/ ObjSetup base;
 /*18*/ s8 yaw;
-/*19*/ s8 unk19;
+/*19*/ s8 alternateModel;
 } SB_SeqDoor_Setup;
 
-static int SB_SeqDoor_anim_callback(Object *self, Object *animObj, AnimObj_Data *animObjData, s8 arg3);
+static int SB_SeqDoor_anim_callback(Object *self, Object *animObj, AnimObj_Data *animObjData, s8 prevCallbackValue);
 
 // offset: 0x0 | ctor
 void SB_SeqDoor_ctor(void *dll) { }
@@ -16,15 +17,15 @@ void SB_SeqDoor_ctor(void *dll) { }
 void SB_SeqDoor_dtor(void *dll) { }
 
 // offset: 0x18 | func: 0 | export: 0
-void SB_SeqDoor_setup(Object *self, SB_SeqDoor_Setup *setup, s32 arg2) {
+void SB_SeqDoor_setup(Object *self, SB_SeqDoor_Setup *setup, s32 reset) {
     self->animCallback = SB_SeqDoor_anim_callback;
     self->srt.yaw = setup->yaw << 8;
-    self->modelInstIdx = setup->unk19 != 0;
+    self->modelInstIdx = setup->alternateModel != 0;
 }
 
 // offset: 0x54 | func: 1 | export: 1
 void SB_SeqDoor_control(Object *self) {
-    self->unkAF |= 0x10;
+    self->unkAF |= ARROW_FLAG_10_Greyed_Out;
 }
 
 // offset: 0x68 | func: 2 | export: 2
@@ -38,7 +39,7 @@ void SB_SeqDoor_print(Object *self, Gfx **gdl, Mtx **mtxs, Vertex **vtxs, Triang
 }
 
 // offset: 0xC8 | func: 4 | export: 4
-void SB_SeqDoor_free(Object *self, s32 a1) { }
+void SB_SeqDoor_free(Object *self, s32 onlySelf) { }
 
 // offset: 0xD8 | func: 5 | export: 5
 u32 SB_SeqDoor_get_model_flags(Object *self) {
@@ -46,12 +47,12 @@ u32 SB_SeqDoor_get_model_flags(Object *self) {
 }
 
 // offset: 0xE8 | func: 6 | export: 6
-u32 SB_SeqDoor_get_data_size(Object *self, u32 a1) {
+u32 SB_SeqDoor_get_data_size(Object *self, u32 offsetAddr) {
     return 0;
 }
 
 // offset: 0xFC | func: 7
-int SB_SeqDoor_anim_callback(Object *self, Object *animObj, AnimObj_Data *animObjData, s8 arg3) {
+int SB_SeqDoor_anim_callback(Object *self, Object *animObj, AnimObj_Data *animObjData, s8 prevCallbackValue) {
     animObjData->unk7A = -2;
     animObjData->unk62 = 0;
     return 0;
