68a3344b890cce19da0a2cd58e676399651e5da7
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2026-07-26 03:22:44 -0400

    DLL 46

---
+++ src/dlls/engine/46/exports.s
@@ -0,0 +1,13 @@
+.option pic2
+.section ".exports"
+.global _exports
+_exports:
+
+# ctor/dtor
+.dword dll_46_ctor
+.dword dll_46_dtor
+
+# export table
+/*0*/ .dword dll_46_Func_18
+/*1*/ .dword dll_46_Func_2C
+/*2*/ .dword dll_46_Func_20
