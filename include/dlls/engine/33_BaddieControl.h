dd32a1324b051d63c1766a2524f5671ec1bbe19d
Banjeoin <e.odrisceoil@gmail.com>
2026-07-28 19:10:57 +0100

    BaddieControl_func_278 docs

--- include/dlls/engine/33_BaddieControl.h
+++ include/dlls/engine/33_BaddieControl.h
@@ -35,7 +35,7 @@ typedef struct {
 /*3AC*/ Object *unk3AC;
 /*3B0*/ u8 unk3B0; //flags
 /*3B1*/ u8 _unk3B1;
-/*3B2*/ u16 unk3B2;
+/*3B2*/ u16 unk3B2; //flags (0x80 is frozen?)
 /*3B4*/ u8 unk3B4;
 /*3B5*/ u8 _unk3B5;
 /*3B6*/ s16 unk3B6; //related to acquiring a target/targetting the player?
@@ -106,7 +106,7 @@ DLL_INTERFACE(DLL_33_BaddieControl) {
 /*1*/ void (*dtor)(void *dll);
 /*2*/ f32 (*func2)(Object* arg0, f32 arg1, f32 arg2, f32 arg3, Object* arg4);
 /*3*/ void (*func3)(Object *obj, ObjFSA_Data *fsa, Baddie *baddie, f32 arg3, f32 arg4);
-/*4*/ void (*func4)(Object* arg0, Object* arg1, u8 arg2, u16* arg3, s16* arg4, u16* arg5);
+/*4*/ void (*func4)(Object* obj, Object* target, u8 turnMultiplier, u16* oTurnAmount, s16* oYawDiff, u16* oDistance);
 /*5*/ s32 (*func5)(Object* obj, ObjFSA_Data* fsa, f32 arg2);
 /*6*/ s32 (*func6)(UNK_PTR *arg0, UNK_PTR *arg1);
 /*7*/ u16 (*func7)(Object* arg0);
--- src/dlls/engine/33_BaddieControl/baddieControl.c
+++ src/dlls/engine/33_BaddieControl/baddieControl.c
@@ -7,6 +7,7 @@
 #include "game/objects/object.h"
 #include "game/objects/object_id.h"
 #include "sys/gfx/animseq.h"
+#include "sys/math.h"
 #include "sys/memory.h"
 #include "sys/objanim.h"
 #include "sys/objects.h"
@@ -95,43 +96,50 @@ s32 BaddieControl_func_264(UNK_PTR *arg0, UNK_PTR *arg1) {
 }
 
 // offset: 0x278 | func: 2 | export: 4
-void BaddieControl_func_278(Object* arg0, Object* arg1, u8 arg2, u16* arg3, s16* arg4, u16* arg5) {
-    Baddie* sp2C;
-    Vec3f sp20;
-    Object* temp_v1;
-    s32 temp_a0;
-    s32 temp_v0;
-    s16 var_a1;
-    s32 var_v1;
-    u16 temp_v0_2;
+void BaddieControl_func_278(Object* obj, Object* target, u8 turnMultiplier, u16* oTurnAmount, s16* oYawDiff, u16* oDistance) {
+    Baddie* baddie;
+    Vec3f delta;
+    s32 angleToTarget;
+    s16 wsObjYaw;
+    s32 yawDiff;
+    u16 yawDiffU;
 
-    sp2C = arg0->data;
-    if ((arg0 == NULL) || (arg1 == NULL)) {
-        *arg3 = 0;
-        *arg4 = 0;
-        *arg5 = 0;
+    baddie = obj->data;
+
+    if ((obj == NULL) || (target == NULL)) {
+        *oTurnAmount = 0;
+        *oYawDiff = 0;
+        *oDistance = 0;
         return;
     }
-    sp20.x = arg1->globalPosition.f[0] - arg0->globalPosition.f[0];
-    sp20.y = arg1->globalPosition.f[1] - arg0->globalPosition.f[1];
-    sp20.z = arg1->globalPosition.f[2] - arg0->globalPosition.f[2];
-    temp_v0 = mathAtan2f(-sp20.x, -sp20.z);
-    if (arg0->parent != NULL) {
-        var_a1 = arg0->srt.yaw + arg0->parent->srt.yaw;
+
+    delta.x = target->globalPosition.f[0] - obj->globalPosition.f[0];
+    delta.y = target->globalPosition.f[1] - obj->globalPosition.f[1];
+    delta.z = target->globalPosition.f[2] - obj->globalPosition.f[2];
+    angleToTarget = mathAtan2f(-delta.x, -delta.z);
+
+    //Get Baddie object's worldSpace yaw, by adding its parent's yaw
+    if (obj->parent != NULL) {
+        wsObjYaw = obj->srt.yaw + obj->parent->srt.yaw;
     } else {
-        var_a1 = arg0->srt.yaw;
+        wsObjYaw = obj->srt.yaw;
     }
-    var_v1 = temp_v0 - (var_a1 & 0xFFFF);
-    CIRCLE_WRAP(var_v1)
-    temp_v0_2 = var_v1;
-    *arg4 = var_v1;
-    if ((temp_v0_2 < 0x31C4) || (temp_v0_2 >= 0xCE3C)) {
-        sp2C->unk3B2 &= ~0x10;
+
+    //Get yawDiff to target
+    yawDiff = angleToTarget - (wsObjYaw & 0xFFFF);
+    CIRCLE_WRAP(yawDiff)
+    yawDiffU = yawDiff;
+    *oYawDiff = yawDiff;
+
+    //Check if yawDiff is within -70 to +70 degree range
+    if ((0x31C4 > yawDiffU) || (yawDiffU >= (M_360_DEGREES - 0x31C4))) {
+        baddie->unk3B2 &= ~0x10;
     } else {
-        sp2C->unk3B2 |= 0x10;
+        baddie->unk3B2 |= 0x10;
     }
-    *arg3 = (temp_v0_2 / (0x10000 / arg2));
-    *arg5 = (u32) VECTOR_MAGNITUDE(sp20);
+
+    *oTurnAmount = yawDiffU / (M_360_DEGREES / turnMultiplier);
+    *oDistance = (u32) VECTOR_MAGNITUDE(delta);
 }
 
 // offset: 0x4EC | func: 3 | export: 2
