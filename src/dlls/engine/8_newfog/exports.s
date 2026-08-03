16b1a722eef9ea47ffb15e7df9819f9eca36a96f
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2026-08-03 12:06:28 -0400

    Fix newfog syms

--- src/dlls/engine/8_newfog/exports.s
+++ src/dlls/engine/8_newfog/exports.s
@@ -4,20 +4,20 @@
 _exports:
 
 # ctor/dtor
-.dword dll_8_ctor
-.dword dll_8_dtor
+.dword newfog_ctor
+.dword newfog_dtor
 
 # export table
-/*0*/ .dword dll_8_Func_A0
-/*1*/ .dword dll_8_Func_464
-/*2*/ .dword dll_8_Func_5AC
-/*3*/ .dword dll_8_Func_1234
-/*4*/ .dword dll_8_Func_139C
-/*5*/ .dword dll_8_Func_13AC
-/*6*/ .dword dll_8_Func_1764
-/*7*/ .dword dll_8_Func_22FC
-/*8*/ .dword dll_8_Func_2350
-/*9*/ .dword dll_8_Func_18C0
-/*10*/ .dword dll_8_Func_14B0
-/*11*/ .dword dll_8_Func_1608
-/*12*/ .dword dll_8_Func_18C8
+/*0*/ .dword newfog_Func_A0
+/*1*/ .dword newfog_Func_464
+/*2*/ .dword newfog_Func_5AC
+/*3*/ .dword newfog_Func_1234
+/*4*/ .dword newfog_Func_139C
+/*5*/ .dword newfog_Func_13AC
+/*6*/ .dword newfog_Func_1764
+/*7*/ .dword newfog_GetColor
+/*8*/ .dword newfog_GetPosition
+/*9*/ .dword newfog_Func_18C0
+/*10*/ .dword newfog_Func_14B0
+/*11*/ .dword newfog_Func_1608
+/*12*/ .dword newfog_Func_18C8
